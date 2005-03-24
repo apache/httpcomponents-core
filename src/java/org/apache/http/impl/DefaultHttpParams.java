754db21ecc9f80a0c3c9ec04bbdf7a3d135adc7d
Oleg Kalnichevski <olegk@apache.org>
2005-03-24 21:23:38 +0000

    Copied from Jakarta Commons HttpClient
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpclient/trunk/http-common@158948 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/http/impl/DefaultHttpParams.java
@@ -0,0 +1,206 @@
+/*
+ * $HeadURL$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ *
+ *  Copyright 1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.http.impl;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+import org.apache.http.params.HttpParams;
+
+/**
+ * This class represents a collection of HTTP protocol parameters. Protocol parameters
+ * may be linked together to form a hierarchy. If a particular parameter value has not been
+ * explicitly defined in the collection itself, its value will be drawn from the parent 
+ * collection of parameters.
+ * 
+ * @author <a href="mailto:oleg at ural.ru">Oleg Kalnichevski</a>
+ * 
+ * @version $Revision$
+ * 
+ * @since 3.0
+ */
+public class DefaultHttpParams implements HttpParams, Serializable, Cloneable {
+
+    /** The set of default values to defer to */
+    private HttpParams defaults = null;
+
+    /** Hash map of HTTP parameters that this collection contains */
+    private HashMap parameters = null;
+    
+    /**
+     * Creates a new collection of parameters with the given parent. 
+     * The collection will defer to its parent for a default value 
+     * if a particular parameter is not explicitly set in the collection
+     * itself.
+     * 
+     * @param defaults the parent collection to defer to, if a parameter
+     * is not explictly set in the collection itself.
+     */
+    public DefaultHttpParams(final HttpParams defaults) {
+        super();
+        this.defaults = defaults; 
+    }
+    
+    public synchronized HttpParams getDefaults() {
+        return this.defaults;
+    }
+    
+    public synchronized void setDefaults(final HttpParams params) {
+        this.defaults = params;
+    }
+    
+    public synchronized Object getParameter(final String name) {
+        // See if the parameter has been explicitly defined
+        Object param = null;
+        if (this.parameters != null) {
+            param = this.parameters.get(name);
+        }    
+        if (param != null) {
+            // If so, return
+            return param;
+        } else {
+            // If not, see if defaults are available
+            if (this.defaults != null) {
+                // Return default parameter value
+                return this.defaults.getParameter(name);
+            } else {
+                // Otherwise, return null
+                return null;
+            }
+        }
+    }
+
+    public synchronized void setParameter(final String name, final Object value) {
+        if (this.parameters == null) {
+            this.parameters = new HashMap();
+        }
+        this.parameters.put(name, value);
+    }
+    
+    /**
+     * Assigns the value to all the parameter with the given names
+     * 
+     * @param names array of parameter name
+     * @param value parameter value
+     */ 
+    public synchronized void setParameters(final String[] names, final Object value) {
+        for (int i = 0; i < names.length; i++) {
+            setParameter(names[i], value);
+        }
+    }
+
+    public long getLongParameter(final String name, long defaultValue) { 
+        Object param = getParameter(name);
+        if (param == null) {
+            return defaultValue;
+        }
+        return ((Long)param).longValue();
+    }
+    
+    public void setLongParameter(final String name, long value) {
+        setParameter(name, new Long(value));
+    }
+
+    public int getIntParameter(final String name, int defaultValue) { 
+        Object param = getParameter(name);
+        if (param == null) {
+            return defaultValue;
+        }
+        return ((Integer)param).intValue();
+    }
+    
+    public void setIntParameter(final String name, int value) {
+        setParameter(name, new Integer(value));
+    }
+
+    public double getDoubleParameter(final String name, double defaultValue) { 
+        Object param = getParameter(name);
+        if (param == null) {
+            return defaultValue;
+        }
+        return ((Double)param).doubleValue();
+    }
+    
+    public void setDoubleParameter(final String name, double value) {
+        setParameter(name, new Double(value));
+    }
+
+    public boolean getBooleanParameter(final String name, boolean defaultValue) { 
+        Object param = getParameter(name);
+        if (param == null) {
+            return defaultValue;
+        }
+        return ((Boolean)param).booleanValue();
+    }
+    
+    public void setBooleanParameter(final String name, boolean value) {
+        setParameter(name, new Boolean(value));
+    }
+
+    public boolean isParameterSet(final String name) {
+        return getParameter(name) != null;
+    }
+        
+    public boolean isParameterSetLocally(final String name) {
+        return this.parameters != null && this.parameters.get(name) != null;
+    }
+        
+    public boolean isParameterTrue(final String name) {
+        return getBooleanParameter(name, false);
+    }
+        
+    public boolean isParameterFalse(final String name) {
+        return !getBooleanParameter(name, false);
+    }
+
+    /**
+     * Removes all parameters from this collection. 
+     */
+    public void clear() {
+        this.parameters = null;
+    }
+
+    /**
+     * Clones this collection of parameters. Please note that paramter values
+     * themselves are not cloned. 
+     * 
+     * @see java.io.Serializable
+     * @see java.lang.Object#clone()
+     */
+    public Object clone() throws CloneNotSupportedException
+    {
+        DefaultHttpParams clone = (DefaultHttpParams)super.clone();
+        if (this.parameters != null) {
+            clone.parameters = (HashMap)this.parameters.clone(); 
+        }
+        clone.setDefaults(this.defaults);
+        return clone;
+    }
+}
---
+++ src/java/org/apache/http/params/HttpParams.java
@@ -0,0 +1,231 @@
+/*
+ * $HeadURL$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ *
+ *  Copyright 1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.http.params;
+
+/**
+ * This interface represents a collection of HTTP protocol parameters. Protocol parameters
+ * may be linked together to form a hierarchy. If a particular parameter value has not been
+ * explicitly defined in the collection itself, its value will be drawn from the parent 
+ * collection of parameters.
+ *   
+ * @author <a href="mailto:oleg at ural.ru">Oleg Kalnichevski</a>
+ * 
+ * @version $Revision$
+ *
+ * @since 3.0
+ */
+public interface HttpParams {
+
+    /** 
+     * Returns the parent collection that this collection will defer to
+     * for a default value if a particular parameter is not explicitly 
+     * set in the collection itself
+     * 
+     * @return the parent collection to defer to, if a particular parameter
+     * is not explictly set in the collection itself.
+     * 
+     * @see #setDefaults(HttpParams)
+     */
+    HttpParams getDefaults();
+
+    /** 
+     * Assigns the parent collection that this collection will defer to
+     * for a default value if a particular parameter is not explicitly 
+     * set in the collection itself
+     * 
+     * @param params the parent collection to defer to, if a particular 
+     * parameter is not explictly set in the collection itself.
+     * 
+     * @see #getDefaults()
+     */
+    void setDefaults(HttpParams params);
+    
+    /** 
+     * Returns a parameter value with the given name. If the parameter is
+     * not explicitly defined in this collection, its value will be drawn 
+     * from a higer level collection at which this parameter is defined.
+     * If the parameter is not explicitly set anywhere up the hierarchy,
+     * <tt>null</tt> value is returned.  
+     * 
+     * @param name the parent name.
+     * 
+     * @return an object that represents the value of the parameter.
+     * 
+     * @see #setParameter(String, Object)
+     */
+    Object getParameter(String name);
+
+    /**
+     * Assigns the value to the parameter with the given name
+     * 
+     * @param name parameter name
+     * @param value parameter value
+     */ 
+    void setParameter(String name, Object value);
+    
+    /** 
+     * Returns a {@link Long} parameter value with the given name. 
+     * If the parameter is not explicitly defined in this collection, its 
+     * value will be drawn from a higer level collection at which this parameter 
+     * is defined. If the parameter is not explicitly set anywhere up the hierarchy,
+     * the default value is returned.  
+     * 
+     * @param name the parent name.
+     * @param defaultValue the default value.
+     * 
+     * @return a {@link Long} that represents the value of the parameter.
+     * 
+     * @see #setLongParameter(String, long)
+     */
+    long getLongParameter(String name, long defaultValue); 
+    
+    /**
+     * Assigns a {@link Long} to the parameter with the given name
+     * 
+     * @param name parameter name
+     * @param value parameter value
+     */ 
+    void setLongParameter(String name, long value);
+
+    /** 
+     * Returns an {@link Integer} parameter value with the given name. 
+     * If the parameter is not explicitly defined in this collection, its 
+     * value will be drawn from a higer level collection at which this parameter 
+     * is defined. If the parameter is not explicitly set anywhere up the hierarchy,
+     * the default value is returned.  
+     * 
+     * @param name the parent name.
+     * @param defaultValue the default value.
+     * 
+     * @return a {@link Integer} that represents the value of the parameter.
+     * 
+     * @see #setIntParameter(String, int)
+     */
+    int getIntParameter(String name, int defaultValue); 
+    
+    /**
+     * Assigns an {@link Integer} to the parameter with the given name
+     * 
+     * @param name parameter name
+     * @param value parameter value
+     */ 
+    void setIntParameter(String name, int value);
+
+    /** 
+     * Returns a {@link Double} parameter value with the given name. 
+     * If the parameter is not explicitly defined in this collection, its 
+     * value will be drawn from a higer level collection at which this parameter 
+     * is defined. If the parameter is not explicitly set anywhere up the hierarchy,
+     * the default value is returned.  
+     * 
+     * @param name the parent name.
+     * @param defaultValue the default value.
+     * 
+     * @return a {@link Double} that represents the value of the parameter.
+     * 
+     * @see #setDoubleParameter(String, double)
+     */
+    double getDoubleParameter(String name, double defaultValue); 
+    
+    /**
+     * Assigns a {@link Double} to the parameter with the given name
+     * 
+     * @param name parameter name
+     * @param value parameter value
+     */ 
+    void setDoubleParameter(String name, double value);
+
+    /** 
+     * Returns a {@link Boolean} parameter value with the given name. 
+     * If the parameter is not explicitly defined in this collection, its 
+     * value will be drawn from a higer level collection at which this parameter 
+     * is defined. If the parameter is not explicitly set anywhere up the hierarchy,
+     * the default value is returned.  
+     * 
+     * @param name the parent name.
+     * @param defaultValue the default value.
+     * 
+     * @return a {@link Boolean} that represents the value of the parameter.
+     * 
+     * @see #setBooleanParameter(String, boolean)
+     */
+    boolean getBooleanParameter(String name, boolean defaultValue); 
+    
+    /**
+     * Assigns a {@link Boolean} to the parameter with the given name
+     * 
+     * @param name parameter name
+     * @param value parameter value
+     */ 
+    void setBooleanParameter(String name, boolean value);
+
+    /**
+     * Returns <tt>true</tt> if the parameter is set at any level, <tt>false</tt> otherwise.
+     * 
+     * @param name parameter name
+     * 
+     * @return <tt>true</tt> if the parameter is set at any level, <tt>false</tt>
+     * otherwise.
+     */
+    boolean isParameterSet(String name);
+        
+    /**
+     * Returns <tt>true</tt> if the parameter is set locally, <tt>false</tt> otherwise.
+     * 
+     * @param name parameter name
+     * 
+     * @return <tt>true</tt> if the parameter is set locally, <tt>false</tt>
+     * otherwise.
+     */
+    boolean isParameterSetLocally(String name);
+        
+    /**
+     * Returns <tt>true</tt> if the parameter is set and is <tt>true</tt>, <tt>false</tt>
+     * otherwise.
+     * 
+     * @param name parameter name
+     * 
+     * @return <tt>true</tt> if the parameter is set and is <tt>true</tt>, <tt>false</tt>
+     * otherwise.
+     */
+    boolean isParameterTrue(String name);
+        
+    /**
+     * Returns <tt>true</tt> if the parameter is either not set or is <tt>false</tt>, 
+     * <tt>false</tt> otherwise.
+     * 
+     * @param name parameter name
+     * 
+     * @return <tt>true</tt> if the parameter is either not set or is <tt>false</tt>, 
+     * <tt>false</tt> otherwise.
+     */
+    boolean isParameterFalse(String name);
+
+}
