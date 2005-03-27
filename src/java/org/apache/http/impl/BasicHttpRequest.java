3f11dd4ee912008054f6dd62e17d8c4c3906f8e6
Oleg Kalnichevski <olegk@apache.org>
2005-03-27 13:45:12 +0000

    Added HttpMutableRequest and HttpMutableResponse interfaces
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpclient/trunk/http-common@159146 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/http/impl/BasicHttpRequest.java
@@ -0,0 +1,87 @@
+/*
+ * $HeadURL$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ *
+ *  Copyright 1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.http.impl;
+
+import org.apache.http.HttpMutableRequest;
+import org.apache.http.HttpVersion;
+import org.apache.http.RequestLine;
+import org.apache.http.params.HttpProtocolParams;
+
+/**
+ * <p>
+ * </p>
+ * @author <a href="mailto:oleg at ural.ru">Oleg Kalnichevski</a>
+ *
+ * @version $Revision$
+ * 
+ * @since 4.0
+ */
+public class BasicHttpRequest extends BasicHttpMessage implements HttpMutableRequest {
+    
+    private String method = null;
+    private String uri = null;
+    private RequestLine requestline = null;
+    
+    protected BasicHttpRequest(final String method, final String uri) {
+        super();
+        if (method == null) {
+            throw new IllegalArgumentException("Method name may not be null");
+        }
+        if (uri == null) {
+            throw new IllegalArgumentException("Request URI may not be null");
+        }
+        this.method = method;
+        this.uri = uri;
+    }
+
+    protected BasicHttpRequest(final RequestLine requestline) {
+        super();
+        setRequestLine(requestline);
+    }
+
+    public RequestLine getRequestLine() {
+        if (this.requestline != null) {
+            return this.requestline;
+        } else {
+            HttpVersion ver = new HttpProtocolParams(getParams()).getVersion();
+            return new RequestLine(this.method, this.uri, ver);
+        }
+    }
+
+    public void setRequestLine(final RequestLine requestline) {
+        if (requestline == null) {
+            throw new IllegalArgumentException("Request line may not be null");
+        }
+        this.requestline = requestline;
+        this.method = requestline.getMethod();
+        this.uri = requestline.getUri();
+    }
+    
+}
