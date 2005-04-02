70a0b5ecc521643e35fe9ae52553fd6ba21bd5fd
Oleg Kalnichevski <olegk@apache.org>
2005-04-02 18:11:35 +0000

    Added HttpDataReceiverFactory and HttpDataTransmitterFactory interfaces and their default impls
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpclient/trunk/http-common@159783 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/http/impl/DefaultHttpDataReceiverFactory.java
@@ -0,0 +1,71 @@
+/*
+ * $HeadURL$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ *
+ *  Copyright 1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.http.impl;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.net.ssl.SSLSocket;
+
+import org.apache.http.HttpRuntime;
+import org.apache.http.io.HttpDataReceiver;
+import org.apache.http.io.HttpDataReceiverFactory;
+
+/**
+ * <p>
+ * </p>
+ * @author <a href="mailto:oleg at ural.ru">Oleg Kalnichevski</a>
+ *
+ * @version $Revision$
+ * 
+ * @since 4.0
+ */
+public class DefaultHttpDataReceiverFactory implements HttpDataReceiverFactory {
+    
+    public HttpDataReceiver create(final Socket socket) throws IOException {
+        if (socket == null) {
+            throw new IllegalArgumentException("Socket may not be null");
+        }
+        if (socket instanceof SSLSocket) {
+            if (HttpRuntime.isSSLNIOCapable()) {
+                return new NIOSocketHttpDataReceiver(socket); 
+            } else {
+                return new OldIOSocketHttpDataReceiver(socket); 
+            }
+        } else {
+            if (HttpRuntime.isNIOCapable()) {
+                return new NIOSocketHttpDataReceiver(socket); 
+            } else {
+                return new OldIOSocketHttpDataReceiver(socket); 
+            }            
+        }
+    }
+    
+}
