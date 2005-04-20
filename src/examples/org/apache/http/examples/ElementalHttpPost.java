1ce6c6e40a4e6a4ee37b388c9b6dacd42cff40cf
Oleg Kalnichevski <olegk@apache.org>
2005-04-20 19:19:53 +0000

    Elemental HTTP POST sample
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpclient/trunk/http-common@162040 13f79535-47bb-0310-9956-ffa450edef68

--- src/examples/org/apache/http/examples/ElementalHttpPost.java
+++ src/examples/org/apache/http/examples/ElementalHttpPost.java
@@ -29,14 +29,20 @@
 
 package org.apache.http.examples;
 
+import java.io.ByteArrayInputStream;
+
 import org.apache.http.Header;
 import org.apache.http.HttpClientConnection;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.entity.EntityConsumer;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.DefaultHttpClientConnection;
 import org.apache.http.impl.DefaultHttpParams;
-import org.apache.http.impl.HttpGetRequest;
+import org.apache.http.impl.HttpPostRequest;
 import org.apache.http.params.HttpParams;
 
 /**
@@ -46,25 +52,46 @@ import org.apache.http.params.HttpParams;
  *
  * @version $Revision$
  */
-public class ElementalHttpGet {
+public class ElementalHttpPost {
 
     public static void main(String[] args) throws Exception {
         
         HttpParams connparams = new DefaultHttpParams(null);
-        HttpHost host = new HttpHost("www.yahoo.com");
+        HttpHost host = new HttpHost("localhost", 8080);
         HttpClientConnection conn = new DefaultHttpClientConnection();
         try {
             
-            String[] targets = {
-                    "/",
-                    "/news/", 
-                    "/somewhere%20in%20pampa"};
+            HttpEntity[] requestBodies = {
+                    new StringEntity(
+                            "This is the first test request", "UTF-8"),
+                    new ByteArrayEntity(
+                            "This is the second test request".getBytes("UTF-8")),
+                    new InputStreamEntity(
+                            new ByteArrayInputStream(
+                                    "This is the third test request (will be chunked)"
+                                    .getBytes("UTF-8")), -1)
+            };
             
-            for (int i = 0; i < targets.length; i++) {
-                HttpGetRequest request = new HttpGetRequest(targets[i]);
+            for (int i = 0; i < requestBodies.length; i++) {
+                HttpPostRequest request = new HttpPostRequest("/httpclienttest/body");
                 request.setHeader(new Header("Host", host.toHostString()));
                 request.setHeader(new Header("Agent", "Elemental HTTP client"));
                 request.setHeader(new Header("Connection", "Keep-Alive"));
+                
+                HttpEntity requestbody = requestBodies[i];
+                // Must specify a transfer encoding or a content length 
+                if (requestbody.isChunked() || requestbody.getContentLength() < 0) {
+                    request.setHeader(new Header("Transfer-Encoding", "chunked"));
+                } else {
+                    request.setHeader(new Header("Content-Length", 
+                            Long.toString(requestbody.getContentLength())));
+                }
+                // Specify a content type if known
+                if (requestbody.getContentType() != null) {
+                    request.setHeader(new Header("Content-Type", requestbody.getContentType())); 
+                }
+                request.setEntity(requestbody);
+                
                 if (!conn.isOpen()) {
                     System.out.println("Open new connection to: " + host);
                     conn.open(host, connparams, null);
@@ -76,7 +103,7 @@ public class ElementalHttpGet {
                 // Request may be terminated prematurely, when expect-continue 
                 // protocol is used
                 if (response == null) {
-                    // No error response so far. 
+                    // No error response so far.  
                     response = conn.receiveResponse(request);
                 }
                 EntityConsumer body = new EntityConsumer(response);
@@ -86,8 +113,7 @@ public class ElementalHttpGet {
             }
         } finally {
             conn.close();
-        }
-        
+        }        
     }
     
 }
