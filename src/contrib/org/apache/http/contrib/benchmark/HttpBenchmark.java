e4f437c4164923b884f71dd6550c8052f685ff63
Oleg Kalnichevski <olegk@apache.org>
2005-12-15 19:21:46 +0000

    Added the standard ASF header
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpcomponents/trunk/http-core@357064 13f79535-47bb-0310-9956-ffa450edef68

--- src/contrib/org/apache/http/contrib/benchmark/HttpBenchmark.java
+++ src/contrib/org/apache/http/contrib/benchmark/HttpBenchmark.java
@@ -1,3 +1,31 @@
+/*
+ * $HeadURL: $
+ * $Revision: $
+ * $Date: $
+ *
+ * ====================================================================
+ *
+ *  Copyright 1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
 package org.apache.http.contrib.benchmark;
 
 import java.io.File;
@@ -30,6 +58,15 @@ import org.apache.http.protocol.RequestExpectContinue;
 import org.apache.http.protocol.RequestTargetHost;
 import org.apache.http.protocol.RequestUserAgent;
 
+/**
+ * <p>
+ * </p>
+ * @author <a href="mailto:oleg at ural.ru">Oleg Kalnichevski</a>
+ *
+ * @version $Revision: $
+ * 
+ * @since 4.0
+ */
 public class HttpBenchmark {
 
     private static HttpRequestExecutor createRequestExecutor() {
