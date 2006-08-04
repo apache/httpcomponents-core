81f22c944cfe1dd4f3059081a5d3fd448638e4df
Oleg Kalnichevski <olegk@apache.org>
2006-08-04 15:50:05 +0000

    Changed the elemental HTTP server demo to make use of the EntityTemplate class
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpcomponents/httpcore/trunk@428761 13f79535-47bb-0310-9956-ffa450edef68

--- src/examples/org/apache/http/examples/ElementalHttpServer.java
+++ src/examples/org/apache/http/examples/ElementalHttpServer.java
@@ -32,6 +32,8 @@ package org.apache.http.examples;
 import java.io.File;
 import java.io.IOException;
 import java.io.InterruptedIOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.URLDecoder;
@@ -42,8 +44,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpServerConnection;
 import org.apache.http.HttpStatus;
 import org.apache.http.MethodNotSupportedException;
+import org.apache.http.entity.ContentProducer;
+import org.apache.http.entity.EntityTemplate;
 import org.apache.http.entity.FileEntity;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.DefaultHttpParams;
 import org.apache.http.impl.DefaultHttpServerConnection;
 import org.apache.http.params.HttpConnectionParams;
@@ -96,22 +99,53 @@ public class ElementalHttpServer {
             String docroot = (String) getContext().getAttribute("server.docroot");
             
             String target = request.getRequestLine().getUri();
-            File file = new File(docroot, URLDecoder.decode(target));
+            
+            final File file = new File(docroot, URLDecoder.decode(target));
             if (!file.exists()) {
+
                 response.setStatusCode(HttpStatus.SC_NOT_FOUND);
-                StringEntity body = new StringEntity("File not found", "UTF-8");
+                EntityTemplate body = new EntityTemplate(new ContentProducer() {
+                    
+                    public void writeTo(final OutputStream outstream) throws IOException {
+                        OutputStreamWriter writer = new OutputStreamWriter(outstream, "UTF-8"); 
+                        writer.write("<html><body><h1>");
+                        writer.write("File ");
+                        writer.write(file.getPath());
+                        writer.write(" not found");
+                        writer.write("</h1></body></html>");
+                        writer.flush();
+                    }
+                    
+                });
+                body.setContentType("text/html; charset=UTF-8");
                 response.setEntity(body);
                 System.out.println("File " + file.getPath() + " not found");
+                
             } else if (!file.canRead() || file.isDirectory()) {
+                
                 response.setStatusCode(HttpStatus.SC_FORBIDDEN);
-                StringEntity body = new StringEntity("Access Denied", "UTF-8");
+                EntityTemplate body = new EntityTemplate(new ContentProducer() {
+                    
+                    public void writeTo(final OutputStream outstream) throws IOException {
+                        OutputStreamWriter writer = new OutputStreamWriter(outstream, "UTF-8"); 
+                        writer.write("<html><body><h1>");
+                        writer.write("Access denied");
+                        writer.write("</h1></body></html>");
+                        writer.flush();
+                    }
+                    
+                });
+                body.setContentType("text/html; charset=UTF-8");
                 response.setEntity(body);
                 System.out.println("Cannot read file " + file.getPath());
+                
             } else {
+                
                 response.setStatusCode(HttpStatus.SC_OK);
                 FileEntity body = new FileEntity(file, "text/html");
                 response.setEntity(body);
                 System.out.println("Serving file " + file.getPath());
+                
             }
         }
         
