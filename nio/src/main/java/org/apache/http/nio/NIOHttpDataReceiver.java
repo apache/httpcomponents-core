ce2737d9d3dd34de84cd6ca1fd90c2d7aa0338de
Oleg Kalnichevski <olegk@apache.org>
2006-08-10 20:21:27 +0000

    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpcomponents/httpcore/trunk@430516 13f79535-47bb-0310-9956-ffa450edef68

---
+++ nio/src/main/java/org/apache/http/nio/NIOHttpDataReceiver.java
@@ -0,0 +1,240 @@
+/*
+ * $HeadURL$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ *
+ *  Copyright 1999-2006 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.http.nio;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CoderResult;
+import java.nio.charset.CodingErrorAction;
+
+import org.apache.http.io.CharArrayBuffer;
+import org.apache.http.io.HttpDataReceiver;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+
+/**
+ * <p>
+ * </p>
+ * @author <a href="mailto:oleg at ural.ru">Oleg Kalnichevski</a>
+ *
+ * @version $Revision$
+ * 
+ * @since 4.0
+ */
+public abstract class NIOHttpDataReceiver implements HttpDataReceiver {
+
+    private ByteBuffer buffer = null;
+    
+    private Charset charset = null;
+    private CharsetDecoder chardecoder = null;
+    private CharBuffer chbuffer = null;
+    
+    protected void initBuffer(int buffersize) {
+        this.buffer = ByteBuffer.allocateDirect(buffersize);
+        this.buffer.flip();
+        this.charset = Charset.forName("US-ASCII");
+        this.chardecoder = createCharDecoder();
+        this.chbuffer = CharBuffer.allocate(1024);
+    }
+
+    public synchronized void reset(final HttpParams params) {
+        this.charset = Charset.forName(HttpProtocolParams.getHttpElementCharset(params)); 
+        this.chardecoder = createCharDecoder();
+    }
+
+    private CharsetDecoder createCharDecoder() {
+        CharsetDecoder chardecoder = this.charset.newDecoder();
+        chardecoder.onMalformedInput(CodingErrorAction.REPLACE); 
+        chardecoder.onUnmappableCharacter(CodingErrorAction.REPLACE);
+        return chardecoder;
+    }
+    
+    private int doFillBuffer() throws IOException {
+        this.buffer.compact();
+        int i = fillBuffer(this.buffer);
+        this.buffer.flip();
+        return i;
+    }
+
+    protected abstract int fillBuffer(ByteBuffer dst) throws IOException;
+    
+    protected synchronized boolean hasDataInBuffer() {
+        return this.buffer.hasRemaining();
+    }
+    
+    public synchronized int read(final byte[] b, int off, int len) throws IOException {
+        if (b == null) {
+            return 0;
+        }
+        int noRead = 0;
+        if (!this.buffer.hasRemaining()) {
+            noRead = doFillBuffer();
+            if (noRead == -1) {
+                return -1; 
+            }
+        }
+        int chunk = len;
+        if (chunk > this.buffer.remaining()) {
+            chunk = this.buffer.remaining();
+        }
+        this.buffer.get(b, off, chunk);
+        return chunk;
+    }
+    
+    public synchronized int read(final byte[] b) throws IOException {
+        if (b == null) {
+            return 0;
+        }
+        return read(b, 0, b.length);
+    }
+    
+    public synchronized int read() throws IOException {
+        int noRead = 0;
+        if (!this.buffer.hasRemaining()) {
+            noRead = doFillBuffer();
+            if (noRead == -1) {
+                return -1; 
+            }
+        }
+        int b = this.buffer.get();
+        if (b < 0) {
+            b = 256 + b;
+        }
+        return b;
+    }
+    
+    private int locateLF() {
+        for (int i = this.buffer.position(); i < this.buffer.limit(); i++) {
+            int b = this.buffer.get(i);
+            if (b == HTTP.LF) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    public synchronized int readLine(final CharArrayBuffer charbuffer) throws IOException {
+        if (charbuffer == null) {
+            throw new IllegalArgumentException("Char array buffer may not be null");
+        }
+        int noRead = 0;
+        this.chbuffer.clear();
+        this.chardecoder.reset();
+        boolean retry = true;
+        while (retry) {
+            // attempt to find end of line (LF)
+            int i = locateLF(); 
+            if (i != -1) {
+                // end of line found. 
+                retry = false;
+                // read up to the end of line
+                int origLimit = this.buffer.limit();
+                this.buffer.limit(i + 1);
+                for (;;) {
+                    CoderResult result = this.chardecoder.decode(
+                    		this.buffer, this.chbuffer, true);
+                    if (result.isOverflow()) {
+                        this.chbuffer.flip();
+                        charbuffer.append(this.chbuffer.array(), 
+                        		this.chbuffer.position(), this.chbuffer.remaining());
+                        this.chbuffer.clear();
+                    }
+                    if (result.isUnderflow()) {
+                        break;
+                    }
+                }
+                this.buffer.limit(origLimit);
+            } else {
+                // end of line not found
+                if (this.buffer.hasRemaining()) {
+                    // decode the entire buffer content
+                    this.chardecoder.decode(this.buffer, this.chbuffer, false);
+                }
+                // discard the decoded content
+                noRead = doFillBuffer();
+                if (noRead == -1) {
+                    retry = false;
+                    // terminate the decoding process
+                    this.chardecoder.decode(this.buffer, this.chbuffer, true);
+                }
+            }
+            // append the decoded content to the line buffer
+            this.chbuffer.flip();
+            if (this.chbuffer.hasRemaining()) {
+                charbuffer.append(this.chbuffer.array(), 
+                		this.chbuffer.position(), this.chbuffer.remaining());
+            }
+            this.chbuffer.clear();
+        }
+        // flush the decoder
+        this.chardecoder.flush(this.chbuffer);
+        this.chbuffer.flip();
+        // append the decoded content to the line buffer
+        if (this.chbuffer.hasRemaining()) {
+            charbuffer.append(this.chbuffer.array(), 
+            		this.chbuffer.position(), this.chbuffer.remaining());
+        }
+        if (noRead == -1 && charbuffer.length() == 0) {
+            // indicate the end of stream
+            return -1;
+        }
+        // discard LF if found
+        int l = charbuffer.length(); 
+        if (l > 0) {
+            if (charbuffer.charAt(l - 1) == HTTP.LF) {
+                l--;
+                charbuffer.setLength(l);
+            }
+            // discard CR if found
+            if (l > 0) {
+                if (charbuffer.charAt(l - 1) == HTTP.CR) {
+                    l--;
+                    charbuffer.setLength(l);
+                }
+            }
+        }
+        return l;
+    }
+    
+    public String readLine() throws IOException {
+        CharArrayBuffer charbuffer = new CharArrayBuffer(64);
+        int l = readLine(charbuffer);
+        if (l != -1) {
+            return charbuffer.toString();
+        } else {
+            return null;
+        }
+    }
+        
+}
---
+++ nio/src/main/java/org/apache/http/nio/NIOHttpDataTransmitter.java
@@ -0,0 +1,201 @@
+/*
+ * $HeadURL$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ *
+ *  Copyright 1999-2006 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.http.nio;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.CoderResult;
+import java.nio.charset.CodingErrorAction;
+
+import org.apache.http.io.CharArrayBuffer;
+import org.apache.http.io.HttpDataTransmitter;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+
+/**
+ * <p>
+ * </p>
+ * @author <a href="mailto:oleg at ural.ru">Oleg Kalnichevski</a>
+ *
+ * @version $Revision$
+ * 
+ * @since 4.0
+ */
+public abstract class NIOHttpDataTransmitter implements HttpDataTransmitter {
+
+    private static final byte[] CRLF = new byte[] {HTTP.CR, HTTP.LF};
+
+    private ByteBuffer buffer = null;
+
+    private Charset charset = null;
+    private CharsetEncoder charencoder = null;
+    private CharBuffer chbuffer = null;
+
+    protected void initBuffer(int buffersize) {
+        this.buffer = ByteBuffer.allocateDirect(buffersize);
+        this.charset = Charset.forName("US-ASCII");
+        this.charencoder = createCharEncoder();
+        this.chbuffer = CharBuffer.allocate(1024);
+    }
+    
+    public synchronized void reset(final HttpParams params) {
+        this.charset = Charset.forName(HttpProtocolParams.getHttpElementCharset(params)); 
+        this.charencoder = createCharEncoder();
+    }
+
+    private void doFlushBuffer() throws IOException {
+        this.buffer.flip();
+        flushBuffer(this.buffer);
+        this.buffer.compact();
+    }
+    
+    protected abstract void flushBuffer(ByteBuffer src) throws IOException;
+    
+    public synchronized void flush() throws IOException {
+        this.buffer.flip();
+        while (this.buffer.hasRemaining()) {
+            flushBuffer(this.buffer);
+        }
+        this.buffer.clear();
+    }
+    
+    private CharsetEncoder createCharEncoder() {
+        CharsetEncoder charencoder = this.charset.newEncoder();
+        charencoder.onMalformedInput(CodingErrorAction.REPLACE); 
+        charencoder.onUnmappableCharacter(CodingErrorAction.REPLACE); 
+        return charencoder; 
+    }
+    
+    public synchronized void write(final byte[] b, int off, int len) throws IOException {
+        if (b == null) {
+            return;
+        }
+        int remaining = len;
+        while (remaining > 0) {
+            if (this.buffer.hasRemaining()) {
+                int chunk = len;
+                if (chunk > remaining) {
+                    chunk = remaining;
+                }
+                if (chunk > this.buffer.remaining()) {
+                    chunk = this.buffer.remaining();
+                }
+                this.buffer.put(b, off, chunk);
+                off += chunk;
+                remaining -= chunk; 
+            } else {
+                doFlushBuffer();
+            }
+        }
+    }
+
+    public void write(final byte[] b) throws IOException {
+        if (b == null) {
+            return;
+        }
+        write(b, 0, b.length);
+    }
+
+    private void writeCRLF() throws IOException {
+        write(CRLF);
+    }
+
+    public synchronized void write(int b) throws IOException {
+        if (!this.buffer.hasRemaining()) {
+            doFlushBuffer();
+        }
+        this.buffer.put((byte)b);
+    }
+
+    public synchronized void writeLine(final CharArrayBuffer buffer) throws IOException {
+        if (buffer == null) {
+            return;
+        }
+        // Do not bother if the buffer is empty
+        if (buffer.length() > 0 ) {
+        	this.charencoder.reset();
+            // transfer the string in small chunks
+            int remaining = buffer.length();
+            int offset = 0;
+            while (remaining > 0) {
+                int l = this.chbuffer.remaining();
+                boolean eol = false;
+                if (remaining < l) {
+                    l = remaining;
+                    // terminate the encoding process
+                    eol = true;
+                }
+                this.chbuffer.put(buffer.buffer(), offset, offset + l);
+                this.chbuffer.flip();
+                
+                boolean retry = true;
+                while (retry) {
+                    CoderResult result = this.charencoder.encode(this.chbuffer, this.buffer, eol);
+                    if (result.isOverflow()) {
+                        doFlushBuffer();
+                    }
+                    retry = !result.isUnderflow();
+                }
+                
+                this.chbuffer.compact();
+                offset += l;
+                remaining -= l;
+            }
+            // flush the encoder
+            boolean retry = true;
+            while (retry) {
+                CoderResult result = this.charencoder.flush(this.buffer);
+                if (result.isOverflow()) {
+                    doFlushBuffer();
+                }
+                retry = !result.isUnderflow();
+            }
+        }
+        writeCRLF();
+    }
+
+    public void writeLine(final String s) throws IOException {
+        if (s == null) {
+            return;
+        }
+        if (s.length() > 0) {
+            CharArrayBuffer tmp = new CharArrayBuffer(s.length());
+            tmp.append(s);
+            writeLine(tmp);
+        } else {
+            write(CRLF);
+        }
+    }
+    
+}
