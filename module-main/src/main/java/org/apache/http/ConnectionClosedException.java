d22e8a97cc416b795952a7a608be6b09427f514c
Ortwin Glueck <oglueck@apache.org>
2006-11-26 18:38:58 +0000

    typo in doc
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpcomponents/httpcore/trunk@479399 13f79535-47bb-0310-9956-ffa450edef68

--- module-main/src/main/java/org/apache/http/ConnectionClosedException.java
+++ module-main/src/main/java/org/apache/http/ConnectionClosedException.java
@@ -45,7 +45,7 @@ public class ConnectionClosedException extends IOException {
 	static final long serialVersionUID = 617550366255636674L;
 	
     /**
-     * Creates a new MethodNotSupportedException with the specified detail message.
+     * Creates a new ConnectionClosedException with the specified detail message.
      * 
      * @param message The exception detail message
      */
