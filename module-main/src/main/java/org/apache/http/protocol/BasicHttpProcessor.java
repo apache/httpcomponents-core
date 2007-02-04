f70134b0fa57a60e794c637eb7516ccc13a59f4e
Roland Weber <rolandw@apache.org>
2007-02-04 12:34:35 +0000

    HTTPCORE-32
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/httpcomponents/httpcore/trunk@503399 13f79535-47bb-0310-9956-ffa450edef68

--- module-main/src/main/java/org/apache/http/protocol/BasicHttpProcessor.java
+++ module-main/src/main/java/org/apache/http/protocol/BasicHttpProcessor.java
@@ -52,80 +52,127 @@ import org.apache.http.HttpResponseInterceptor;
  * 
  * @since 4.0
  */
-public class BasicHttpProcessor implements HttpProcessor {
+public class BasicHttpProcessor implements
+    HttpProcessor, HttpRequestInterceptorList, HttpResponseInterceptorList {
 
     private List requestInterceptors = null; 
-    private List responseInterceptors = null; 
-    
-    public void addInterceptor(final HttpRequestInterceptor interceptor) {
-        if (interceptor == null) {
+    private List responseInterceptors = null;
+
+
+    // non-Javadoc, see interface HttpRequestInterceptorList
+    public void addRequestInterceptor(final HttpRequestInterceptor itcp) {
+
+        if (itcp == null) {
             return;
         }
         if (this.requestInterceptors == null) {
             this.requestInterceptors = new ArrayList();
         }
-        this.requestInterceptors.add(interceptor);
+        this.requestInterceptors.add(itcp);
     }
-    
-    public void removeInterceptor(final HttpRequestInterceptor interceptor) {
-        if (interceptor == null) {
-            return;
-        }
-        if (this.requestInterceptors == null) {
-            return;
-        }
-        this.requestInterceptors.remove(interceptor);
-        if (this.requestInterceptors.isEmpty()) {
-            this.requestInterceptors = null;
-        }
+
+    /**
+     * Same as {@link #addRequestInterceptor addRequestInterceptor}.
+     *
+     * @param interceptor       the interceptor to add
+     */
+    public final
+        void addInterceptor(final HttpRequestInterceptor interceptor) {
+        addRequestInterceptor(interceptor);
     }
-    
-    public void addInterceptor(final HttpResponseInterceptor interceptor) {
-        if (interceptor == null) {
+
+
+    // non-Javadoc, see interface HttpRequestInterceptorList
+    public int getRequestInterceptorCount() {
+        return (this.requestInterceptors == null) ?
+            0 : this.requestInterceptors.size();
+    }
+
+
+    // non-Javadoc, see interface HttpRequestInterceptorList
+    public HttpRequestInterceptor getRequestInterceptor(int index) {
+        
+        if ((this.requestInterceptors == null) ||
+            (index < 0) || (index >= this.requestInterceptors.size()))
+            return null;
+
+        return (HttpRequestInterceptor) this.requestInterceptors.get(index);
+    }
+
+
+    // non-Javadoc, see interface HttpRequestInterceptorList
+    public void clearRequestInterceptors() {
+        this.requestInterceptors = null;
+    }
+
+
+
+    // non-Javadoc, see interface HttpResponseInterceptorList
+    public void addResponseInterceptor(final HttpResponseInterceptor itcp) {
+        if (itcp == null) {
             return;
         }
         if (this.responseInterceptors == null) {
             this.responseInterceptors = new ArrayList();
         }
-        this.responseInterceptors.add(interceptor);
-    }
-    
-    public void removeInterceptor(final HttpResponseInterceptor interceptor) {
-        if (interceptor == null) {
-            return;
-        }
-        if (this.responseInterceptors == null) {
-            return;
-        }
-        this.responseInterceptors.remove(interceptor);
-        if (this.responseInterceptors.isEmpty()) {
-            this.responseInterceptors = null;
-        }
-    }
-    
-    public void removeInterceptors(final Class clazz) {
-        if (clazz == null) {
-            return;
-        }
-        if (this.requestInterceptors != null) {
-            for (Iterator i = this.requestInterceptors.iterator(); i.hasNext(); ) {
-                if (clazz.isInstance(i.next())) {
-                    i.remove();
-                }
-            }
-        }
-        if (this.responseInterceptors != null) {
-            for (Iterator i = this.responseInterceptors.iterator(); i.hasNext(); ) {
-                if (clazz.isInstance(i.next())) {
-                    i.remove();
-                }
-            }
-        }
+        this.responseInterceptors.add(itcp);
     }
 
+    /**
+     * Same as {@link #addResponseInterceptor addResponseInterceptor}.
+     *
+     * @param interceptor       the interceptor to add
+     */
+    public final
+        void addInterceptor(final HttpResponseInterceptor interceptor) {
+        addResponseInterceptor(interceptor);
+    }
+
+
+    // non-Javadoc, see interface HttpResponseInterceptorList
+    public int getResponseInterceptorCount() {
+        return (this.responseInterceptors == null) ?
+            0 : this.responseInterceptors.size();
+    }
+
+
+    // non-Javadoc, see interface HttpResponseInterceptorList
+    public HttpResponseInterceptor getResponseInterceptor(int index) {
+        
+        if ((this.responseInterceptors == null) ||
+            (index < 0) || (index >= this.responseInterceptors.size()))
+            return null;
+
+        return (HttpResponseInterceptor) this.responseInterceptors.get(index);
+    }
+
+
+    // non-Javadoc, see interface HttpResponseInterceptorList
+    public void clearResponseInterceptors() {
+        this.responseInterceptors = null;
+    }
+
+
+    /**
+     * Sets the interceptor lists.
+     * First, both interceptor lists maintained by this processor
+     * will be cleared.
+     * Subsequently,
+     * elements of the argument list that are request interceptors will be
+     * added to the request interceptor list.
+     * Elements that are response interceptors will be
+     * added to the response interceptor list.
+     * Elements that are both request and response interceptor will be
+     * added to both lists.
+     * Elements that are neither request nor response interceptor
+     * will be ignored.
+     *
+     * @param list      the list of request and response interceptors
+     *                  from which to initialize
+     */
     public void setInterceptors(final List list) {
         if (list == null) {
-            return;
+            throw new IllegalArgumentException("List must not be null.");
         }
         if (this.requestInterceptors != null) {
             this.requestInterceptors.clear();
@@ -143,12 +190,16 @@ public class BasicHttpProcessor implements HttpProcessor {
             }
         }
     }
-    
+
+    /**
+     * Clears both interceptor lists maintained by this processor.
+     */
     public void clearInterceptors() {
-        this.requestInterceptors = null;
-        this.responseInterceptors = null;
+        clearRequestInterceptors();
+        clearResponseInterceptors();
     }
-    
+
+    // non-Javadoc, see interface HttpRequestInterceptor (via HttpProcessor)
     public void process(
             final HttpRequest request,
             final HttpContext context) 
@@ -161,6 +212,7 @@ public class BasicHttpProcessor implements HttpProcessor {
         }
     }
 
+    // non-Javadoc, see interface HttpResponseInterceptor (via HttpProcessor)
     public void process(
             final HttpResponse response,
             final HttpContext context) 
---
+++ module-main/src/main/java/org/apache/http/protocol/HttpRequestInterceptorList.java
@@ -0,0 +1,107 @@
+/*
+ * $HeadURL$
+ * $Revision$
+ * $Date$
+ *
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.http.protocol;
+
+
+import java.util.List;
+
+import org.apache.http.HttpRequestInterceptor;
+
+
+
+/**
+ * Provides access to an ordered list of request interceptors.
+ * Lists are expected to be built upfront and used read-only afterwards
+ * for {@link HttpProcessor processing}.
+ *
+ * @author <a href="mailto:rolandw at apache.org">Roland Weber</a>
+ *
+ * @version $Revision$
+ * 
+ * @since 4.0
+ */
+public interface HttpRequestInterceptorList {
+
+    /**
+     * Appends an interceptor to this list.
+     *
+     * @param itcp      the request interceptor to add, or
+     *                  <code>null</code> to do nothing
+     */
+    void addRequestInterceptor(HttpRequestInterceptor itcp)
+        ;
+
+
+    /**
+     * Obtains the current size of this list.
+     *
+     * @return  the number of request interceptors in this list
+     */
+    int getRequestInterceptorCount()
+        ;
+
+
+    /**
+     * Obtains an interceptor of this list.
+     *
+     * @param index     the index of the interceptor to obtain,
+     *                  0 for first
+     *
+     * @return  the interceptor at the given index, or
+     *          <code>null</code> if the index is out of range
+     */
+    HttpRequestInterceptor getRequestInterceptor(int index)
+        ;
+
+
+    /**
+     * Removes all interceptors from this list.
+     */
+    void clearRequestInterceptors()
+        ;
+
+
+    /**
+     * Sets the interceptors in this list.
+     * This list will be cleared and re-initialized to contain
+     * all request interceptors from the argument list.
+     * If the argument list includes elements that are not request
+     * interceptors, the behavior is implementation dependent.
+     *
+     * @param itcps     the list of request interceptors
+     */
+    void setInterceptors(List itcps)
+        ;
+
+
+} // interface HttpRequestInterceptorList
+
