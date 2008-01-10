0d63d62e924a476b7070e492b5f93885e243a691
Oleg Kalnichevski <olegk@apache.org>
2008-01-10 12:26:57 +0000

    HTTPCORE-137: DefaultHttpRequestFactory extended to support all methods specified in RFC 2616 (except CONNECT)
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@610772 13f79535-47bb-0310-9956-ffa450edef68

--- module-main/src/main/java/org/apache/http/impl/DefaultHttpRequestFactory.java
+++ module-main/src/main/java/org/apache/http/impl/DefaultHttpRequestFactory.java
@@ -49,8 +49,34 @@ import org.apache.http.message.BasicHttpRequest;
  */
 public class DefaultHttpRequestFactory implements HttpRequestFactory {
     
+    private static final String[] RFC2616_COMMON_METHODS = {
+        "GET"
+    };
+    
+    private static final String[] RFC2616_ENTITY_ENC_METHODS = {
+        "POST",
+        "PUT"
+    };
+
+    private static final String[] RFC2616_SPECIAL_METHODS = {
+        "HEAD",
+        "OPTIONS",
+        "DELETE",
+        "TRACE"
+    };
+    
+    
     public DefaultHttpRequestFactory() {
         super();
+    }
+    
+    private static boolean isOneOf(final String[] methods, final String method) {
+        for (int i = 0; i < methods.length; i++) {
+            if (methods[i].equalsIgnoreCase(method)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public HttpRequest newHttpRequest(final RequestLine requestline)
@@ -59,12 +85,12 @@ public class DefaultHttpRequestFactory implements HttpRequestFactory {
             throw new IllegalArgumentException("Request line may not be null");
         }
         String method = requestline.getMethod();
-        if ("GET".equalsIgnoreCase(method)) {
+        if (isOneOf(RFC2616_COMMON_METHODS, method)) {
             return new BasicHttpRequest(requestline); 
-        } else if ("HEAD".equalsIgnoreCase(method)) {
-            return new BasicHttpRequest(requestline); 
-        } else if ("POST".equalsIgnoreCase(method)) {
+        } else if (isOneOf(RFC2616_ENTITY_ENC_METHODS, method)) {
             return new BasicHttpEntityEnclosingRequest(requestline); 
+        } else if (isOneOf(RFC2616_SPECIAL_METHODS, method)) {
+            return new BasicHttpRequest(requestline); 
         } else { 
             throw new MethodNotSupportedException(method +  " method not supported");
         }
@@ -72,10 +98,12 @@ public class DefaultHttpRequestFactory implements HttpRequestFactory {
 
     public HttpRequest newHttpRequest(final String method, final String uri)
 			throws MethodNotSupportedException {
-		if ("GET".equalsIgnoreCase(method)) {
-			return new BasicHttpRequest(method, uri);
-		} else if ("POST".equalsIgnoreCase(method)) {
-			return new BasicHttpEntityEnclosingRequest(method, uri);
+        if (isOneOf(RFC2616_COMMON_METHODS, method)) {
+            return new BasicHttpRequest(method, uri); 
+        } else if (isOneOf(RFC2616_ENTITY_ENC_METHODS, method)) {
+            return new BasicHttpEntityEnclosingRequest(method, uri); 
+        } else if (isOneOf(RFC2616_SPECIAL_METHODS, method)) {
+            return new BasicHttpRequest(method, uri); 
 		} else {
 			throw new MethodNotSupportedException(method
 					+ " method not supported");
