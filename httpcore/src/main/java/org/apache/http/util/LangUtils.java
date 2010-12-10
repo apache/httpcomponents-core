2fd4a937b067aeea08fb282fb0bc64f188eef881
Sebastian Bazley <sebb@apache.org>
2010-12-10 19:53:54 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1044493 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore/src/main/java/org/apache/http/util/LangUtils.java
+++ httpcore/src/main/java/org/apache/http/util/LangUtils.java
@@ -55,10 +55,32 @@ public final class LangUtils {
         return hashCode(seed, obj != null ? obj.hashCode() : 0);
     }
 
+    /**
+     * Check if two objects are equal.
+     * 
+     * @param obj1 first object to compare, may be {@code null}
+     * @param obj2 second object to compare, may be {@code null}
+     * @return {@code true} if the objects are equal or both null
+     */
     public static boolean equals(final Object obj1, final Object obj2) {
         return obj1 == null ? obj2 == null : obj1.equals(obj2);
     }
 
+    /**
+     * Check if two object arrays are equal.
+     * <p>
+     * <ul>
+     * <li>If both parameters are null, return {@code true}</li>
+     * <li>If one parameter is null, return {@code false}</li>
+     * <li>If the array lengths are different, return {@code false}</li>
+     * <li>Compare array elements using .equals(); return {@code false} if any comparisons fail.</li>
+     * <li>Return {@code true}</li>
+     * </ul>
+     *
+     * @param a1 first array to compare, may be {@code null}
+     * @param a2 second array to compare, may be {@code null}
+     * @return {@code true} if the arrays are equal or both null
+     */
     public static boolean equals(final Object[] a1, final Object[] a2) {
         if (a1 == null) {
             if (a2 == null) {
