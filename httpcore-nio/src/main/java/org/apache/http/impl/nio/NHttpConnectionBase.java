apache/httpcomponents-core

commit 79ce9090e79c13655e13a2215b0db77cca55208f

Added protected #bind method allowing connections to be upgraded (bound to another I/O session); improved #toString() method for default HTTP connection implementations
git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1053669 13f79535-47bb-0310-9956-ffa450edef68

httpcore-nio/src/main/java/org/apache/http/impl/nio/NHttpConnectionBase.java
@@ -89,8 +89,6 @@ import org.apache.http.protocol.HttpContext;
 public class NHttpConnectionBase
         implements NHttpConnection, HttpInetConnection, SessionBufferStatus {
 
-    protected final HttpContext context;
-
     protected final ContentLengthStrategy incomingContentStrategy;
     protected final ContentLengthStrategy outgoingContentStrategy;
 
@@ -101,7 +99,9 @@ public class NHttpConnectionBase
     protected final HttpTransportMetricsImpl outTransportMetrics;
     protected final HttpConnectionMetricsImpl connMetrics;
 
+    protected HttpContext context;
     protected IOSession session;
+    protected SocketAddress remote;
     protected volatile ContentDecoder contentDecoder;
     protected volatile boolean hasBufferedInput;
     protected volatile ContentEncoder contentEncoder;
@@ -129,8 +129,6 @@ public class NHttpConnectionBase
         if (params == null) {
             throw new IllegalArgumentException("HTTP params may not be null");
         }
-        this.session = session;
-        this.context = new SessionHttpContext(session);
 
         int buffersize = HttpConnectionParams.getSocketBufferSize(params);
         int linebuffersize = buffersize;
@@ -150,9 +148,26 @@ public class NHttpConnectionBase
                 this.inTransportMetrics,
                 this.outTransportMetrics);
 
-        this.session.setBufferStatus(this);
-        this.session.setEvent(EventMask.READ);
+        setSession(session);
         this.status = ACTIVE;
+    }
+
+    private void setSession(final IOSession session) {
+        this.session = session;
+        this.context = new SessionHttpContext(this.session);
+        this.session.setBufferStatus(this);
+        this.remote = this.session.getRemoteAddress();
+    }
+
+    /**
+     * @since 4.1.1
+     */
+    protected void bind(final IOSession session) {
+        if (session == null) {
+            throw new IllegalArgumentException("I/O session may not be null");
+        }
+        this.session.setBufferStatus(null);
+        setSession(session);
     }
 
     /**
@@ -404,6 +419,18 @@ public class NHttpConnectionBase
 
     public HttpConnectionMetrics getMetrics() {
         return this.connMetrics;
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("[");
+        buffer.append(this.remote);
+        if (this.status == CLOSED) {
+            buffer.append("(closed)");
+        }
+        buffer.append("]");
+        return buffer.toString();
     }
 
 }

httpcore-nio/src/main/java/org/apache/http/impl/nio/DefaultNHttpClientConnection.java
@@ -254,17 +254,4 @@ public class DefaultNHttpClientConnection
         return this.request != null;
     }
 
-    @Override
-    public String toString() {
-        StringBuffer buffer = new StringBuffer();
-        buffer.append("[");
-        if (isOpen()) {
-            buffer.append(this.session.getRemoteAddress());
-        } else {
-            buffer.append("closed");
-        }
-        buffer.append("]");
-        return buffer.toString();
-    }
-
 }

httpcore-nio/src/main/java/org/apache/http/impl/nio/DefaultNHttpServerConnection.java
@@ -256,17 +256,4 @@ public class DefaultNHttpServerConnection
         return this.response != null;
     }
 
-    @Override
-    public String toString() {
-        StringBuffer buffer = new StringBuffer();
-        buffer.append("[");
-        if (isOpen()) {
-            buffer.append(this.session.getRemoteAddress());
-        } else {
-            buffer.append("closed");
-        }
-        buffer.append("]");
-        return buffer.toString();
-    }
-
 }

httpcore-nio/src/main/java/org/apache/http/impl/nio/reactor/IOSessionImpl.java
@@ -272,7 +272,7 @@ public class IOSessionImpl implements IOSession {
         StringBuffer buffer = new StringBuffer();
         buffer.append("[");
         if (this.key.isValid()) {
-            buffer.append("interested ops: ");
+            buffer.append("interest ops: ");
             formatOps(buffer, this.interestOpsCallback != null ?
                     this.currentEventMask : this.key.interestOps());
             buffer.append("; ready ops: ");

commit 85263c20b75d5e984a2aba5b6cf574a4967d3b62

HTTPCORE-282: Newly created non-blocking I/O sessions to have READ interest op bit cleared per default due to a regression introduced by r1053669 (merged from trunk) Contributed by Sadeep Jayasumana <sadeep at wso2.com>
git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/branches/4.1.x@1204889 13f79535-47bb-0310-9956-ffa450edef68

.gitignore
@@ -1,0 +1,6 @@
+.classpath
+.project
+.settings
+.clover
+.externalToolBuilders
+target

httpcore-nio/src/main/java/org/apache/http/impl/nio/NHttpConnectionBase.java
@@ -156,6 +156,7 @@ public class NHttpConnectionBase
         this.session = session;
         this.context = new SessionHttpContext(this.session);
         this.session.setBufferStatus(this);
+        this.session.setEvent(EventMask.READ);
         this.remote = this.session.getRemoteAddress();
     }
 
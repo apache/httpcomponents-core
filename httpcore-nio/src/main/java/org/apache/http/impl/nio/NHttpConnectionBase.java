85263c20b75d5e984a2aba5b6cf574a4967d3b62
Oleg Kalnichevski <olegk@apache.org>
2011-11-22 09:59:08 +0000

    HTTPCORE-282: Newly created non-blocking I/O sessions to have READ interest op bit cleared per default due to a regression introduced by r1053669 (merged from trunk) Contributed by Sadeep Jayasumana <sadeep at wso2.com>
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/branches/4.1.x@1204889 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore-nio/src/main/java/org/apache/http/impl/nio/NHttpConnectionBase.java
+++ httpcore-nio/src/main/java/org/apache/http/impl/nio/NHttpConnectionBase.java
@@ -156,6 +156,7 @@ public class NHttpConnectionBase
         this.session = session;
         this.context = new SessionHttpContext(this.session);
         this.session.setBufferStatus(this);
+        this.session.setEvent(EventMask.READ);
         this.remote = this.session.getRemoteAddress();
     }
 
