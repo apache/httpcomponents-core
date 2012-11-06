da414301dc8d79ae3dc963c22956781e24154155
Gary D. Gregory <ggregory@apache.org>
2012-11-06 20:19:03 +0000

    Javadoc.
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1406304 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore/src/main/java/org/apache/http/pool/PoolStats.java
+++ httpcore/src/main/java/org/apache/http/pool/PoolStats.java
@@ -30,9 +30,11 @@ import org.apache.http.annotation.Immutable;
 
 /**
  * Pool statistics.
- *
+ * <p>
+ * The total number of connections in the pool is equal to {@code available} plus {@code leased}.
+ * </p>
+ * 
  * @since 4.2
- *
  */
 @Immutable
 public class PoolStats {
@@ -50,18 +52,46 @@ public class PoolStats {
         this.max = max;
     }
 
+	/**
+	 * Gets the number of persistent connections tracked by the connection manager currently being used to execute
+	 * requests.
+	 * <p>
+	 * The total number of connections in the pool is equal to {@code available} plus {@code leased}.
+	 * </p>
+	 * 
+	 * @return the number of persistent connections.
+	 */
     public int getLeased() {
         return this.leased;
     }
 
+	/**
+	 * Gets the number of connection requests being blocked awaiting a free connection. This can happen only if there
+	 * are more worker threads contending for fewer connections.
+	 * 
+	 * @return the number of connection requests being blocked awaiting a free connection.
+	 */
     public int getPending() {
         return this.pending;
     }
 
+	/**
+	 * Gets the number idle persistent connections.
+	 * <p>
+	 * The total number of connections in the pool is equal to {@code available} plus {@code leased}.
+	 * </p>
+	 * 
+	 * @return number idle persistent connections.
+	 */
     public int getAvailable() {
         return this.available;
     }
 
+	/**
+	 * Gets the maximum number of allowed persistent connections.
+	 * 
+	 * @return the maximum number of allowed persistent connections.
+	 */
     public int getMax() {
         return this.max;
     }
