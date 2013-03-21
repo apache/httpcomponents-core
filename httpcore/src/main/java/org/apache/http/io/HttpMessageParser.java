fd02f99fb35a0753089015f2d3edd548c1e3c78c
Gary D. Gregory <ggregory@apache.org>
2013-03-21 14:06:21 +0000

    Add missing Javadoc tag.
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1459322 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore/src/main/java/org/apache/http/io/HttpMessageParser.java
+++ httpcore/src/main/java/org/apache/http/io/HttpMessageParser.java
@@ -33,9 +33,11 @@ import org.apache.http.HttpException;
 import org.apache.http.HttpMessage;
 
 /**
- * Abstract message parser intended to build HTTP messages from an arbitrary
- * data source.
- *
+ * Abstract message parser intended to build HTTP messages from an arbitrary data source.
+ * 
+ * @param <T>
+ *            {@link HttpMessage} or a subclass
+ * 
  * @since 4.0
  */
 public interface HttpMessageParser<T extends HttpMessage> {
