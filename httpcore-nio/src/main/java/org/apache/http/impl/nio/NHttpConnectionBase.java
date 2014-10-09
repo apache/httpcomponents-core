c5d63bf74fa36066e413b8ee0e7e63bfceaf570f
Oleg Kalnichevski <olegk@apache.org>
2014-10-09 19:10:01 +0000

    Do not set buffer status callback to null when re-binding non-blocking connection to upgraded I/O session
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1630558 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore-nio/src/main/java/org/apache/http/impl/nio/NHttpConnectionBase.java
+++ httpcore-nio/src/main/java/org/apache/http/impl/nio/NHttpConnectionBase.java
@@ -285,7 +285,6 @@ public class NHttpConnectionBase
      */
     protected void bind(final IOSession session) {
         Args.notNull(session, "I/O session");
-        this.session.setBufferStatus(null);
         setSession(session);
     }
 
