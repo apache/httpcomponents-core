791dfda4280795064059c3a94699df9b0e4df73d
Oleg Kalnichevski <olegk@apache.org>
2014-11-04 20:03:17 +0000

    TokenParser to use CharSequence instead of CharArrayBuffer
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1636717 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore/src/main/java/org/apache/http/message/TokenParser.java
+++ httpcore/src/main/java/org/apache/http/message/TokenParser.java
@@ -30,7 +30,7 @@ package org.apache.http.message;
 import java.util.BitSet;
 
 import org.apache.http.annotation.Immutable;
-import org.apache.http.util.CharArrayBuffer;
+import org.apache.http.util.Args;
 
 /**
  * Low level parser for header field elements. The parsing routines of this class are designed
@@ -84,7 +84,9 @@ public class TokenParser {
      * @param delimiters set of delimiting characters. Can be {@code null} if the token
      *  is not delimited by any character.
      */
-    public String parseToken(final CharArrayBuffer buf, final ParserCursor cursor, final BitSet delimiters) {
+    public String parseToken(final CharSequence buf, final ParserCursor cursor, final BitSet delimiters) {
+        Args.notNull(buf, "Char sequence");
+        Args.notNull(cursor, "Parser cursor");
         final StringBuilder dst = new StringBuilder();
         boolean whitespace = false;
         while (!cursor.atEnd()) {
@@ -115,7 +117,9 @@ public class TokenParser {
      * @param delimiters set of delimiting characters. Can be {@code null} if the value
      *  is not delimited by any character.
      */
-    public String parseValue(final CharArrayBuffer buf, final ParserCursor cursor, final BitSet delimiters) {
+    public String parseValue(final CharSequence buf, final ParserCursor cursor, final BitSet delimiters) {
+        Args.notNull(buf, "Char sequence");
+        Args.notNull(cursor, "Parser cursor");
         final StringBuilder dst = new StringBuilder();
         boolean whitespace = false;
         while (!cursor.atEnd()) {
@@ -149,7 +153,9 @@ public class TokenParser {
      * @param buf buffer with the sequence of chars to be parsed
      * @param cursor defines the bounds and current position of the buffer
      */
-    public void skipWhiteSpace(final CharArrayBuffer buf, final ParserCursor cursor) {
+    public void skipWhiteSpace(final CharSequence buf, final ParserCursor cursor) {
+        Args.notNull(buf, "Char sequence");
+        Args.notNull(cursor, "Parser cursor");
         int pos = cursor.getPos();
         final int indexFrom = cursor.getPos();
         final int indexTo = cursor.getUpperBound();
@@ -174,8 +180,11 @@ public class TokenParser {
      *  is delimited by a whitespace only.
      * @param dst destination buffer
      */
-    public void copyContent(final CharArrayBuffer buf, final ParserCursor cursor, final BitSet delimiters,
+    public void copyContent(final CharSequence buf, final ParserCursor cursor, final BitSet delimiters,
             final StringBuilder dst) {
+        Args.notNull(buf, "Char sequence");
+        Args.notNull(cursor, "Parser cursor");
+        Args.notNull(dst, "String builder");
         int pos = cursor.getPos();
         final int indexFrom = cursor.getPos();
         final int indexTo = cursor.getUpperBound();
@@ -201,8 +210,11 @@ public class TokenParser {
      *  is delimited by a whitespace or a quote only.
      * @param dst destination buffer
      */
-    public void copyUnquotedContent(final CharArrayBuffer buf, final ParserCursor cursor,
+    public void copyUnquotedContent(final CharSequence buf, final ParserCursor cursor,
             final BitSet delimiters, final StringBuilder dst) {
+        Args.notNull(buf, "Char sequence");
+        Args.notNull(cursor, "Parser cursor");
+        Args.notNull(dst, "String builder");
         int pos = cursor.getPos();
         final int indexFrom = cursor.getPos();
         final int indexTo = cursor.getUpperBound();
@@ -226,8 +238,11 @@ public class TokenParser {
      * @param cursor defines the bounds and current position of the buffer
      * @param dst destination buffer
      */
-    public void copyQuotedContent(final CharArrayBuffer buf, final ParserCursor cursor,
+    public void copyQuotedContent(final CharSequence buf, final ParserCursor cursor,
             final StringBuilder dst) {
+        Args.notNull(buf, "Char sequence");
+        Args.notNull(cursor, "Parser cursor");
+        Args.notNull(dst, "String builder");
         if (cursor.atEnd()) {
             return;
         }
