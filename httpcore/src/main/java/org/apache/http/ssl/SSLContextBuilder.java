50c23af46c5045faaecb5bfedda6002aa0f1373a
Sebastian Bazley <sebb@apache.org>
2014-12-09 14:40:02 +0000

    Immutable private field might as well be final
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/branches/4.4.x@1644068 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore/src/main/java/org/apache/http/ssl/SSLContextBuilder.java
+++ httpcore/src/main/java/org/apache/http/ssl/SSLContextBuilder.java
@@ -77,8 +77,8 @@ public class SSLContextBuilder {
     static final String TLS   = "TLS";
 
     private String protocol;
-    private Set<KeyManager> keymanagers;
-    private Set<TrustManager> trustmanagers;
+    private final Set<KeyManager> keymanagers;
+    private final Set<TrustManager> trustmanagers;
     private SecureRandom secureRandom;
 
     public static SSLContextBuilder create() {
