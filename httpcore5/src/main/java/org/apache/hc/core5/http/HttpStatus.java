883c279f80b41c97725c39c16324ad9507aa14d0
Oleg Kalnichevski <olegk@apache.org>
2016-09-18 13:57:06 +0000

    Declaration cleanup and javadoc improvements
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1761330 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore5/src/main/java/org/apache/hc/core5/http/HttpStatus.java
+++ httpcore5/src/main/java/org/apache/hc/core5/http/HttpStatus.java
@@ -29,10 +29,8 @@ package org.apache.hc.core5.http;
 
 /**
  * Constants enumerating the HTTP status codes.
- * All status codes defined in RFC1945 (HTTP/1.0), RFC2616 (HTTP/1.1), and
- * RFC2518 (WebDAV) are listed.
- *
- * @see StatusLine
+ * All status codes defined in RFC1945 (HTTP/1.0), RFC2616 (HTTP/1.1),
+ * RFC2518 (WebDAV) and RFC7540 (HTTP/2) are listed.
  *
  * @since 4.0
  */
@@ -40,101 +38,101 @@ public interface HttpStatus {
 
     // --- 1xx Informational ---
     /** {@code 100 1xx Informational} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_INFORMATIONAL = 100;
+    int SC_INFORMATIONAL = 100;
 
     /** {@code 100 Continue} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_CONTINUE = 100;
+    int SC_CONTINUE = 100;
     /** {@code 101 Switching Protocols} (HTTP/1.1 - RFC 2616)*/
-    public static final int SC_SWITCHING_PROTOCOLS = 101;
+    int SC_SWITCHING_PROTOCOLS = 101;
     /** {@code 102 Processing} (WebDAV - RFC 2518) */
-    public static final int SC_PROCESSING = 102;
+    int SC_PROCESSING = 102;
 
     // --- 2xx Success ---
     /** {@code 2xx Success} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_SUCCESS = 200;
+    int SC_SUCCESS = 200;
 
     /** {@code 200 OK} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_OK = 200;
+    int SC_OK = 200;
     /** {@code 201 Created} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_CREATED = 201;
+    int SC_CREATED = 201;
     /** {@code 202 Accepted} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_ACCEPTED = 202;
+    int SC_ACCEPTED = 202;
     /** {@code 203 Non Authoritative Information} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_NON_AUTHORITATIVE_INFORMATION = 203;
+    int SC_NON_AUTHORITATIVE_INFORMATION = 203;
     /** {@code 204 No Content} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_NO_CONTENT = 204;
+    int SC_NO_CONTENT = 204;
     /** {@code 205 Reset Content} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_RESET_CONTENT = 205;
+    int SC_RESET_CONTENT = 205;
     /** {@code 206 Partial Content} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_PARTIAL_CONTENT = 206;
+    int SC_PARTIAL_CONTENT = 206;
     /**
      * {@code 207 Multi-Status} (WebDAV - RFC 2518)
      * or
      * {@code 207 Partial Update OK} (HTTP/1.1 - draft-ietf-http-v11-spec-rev-01?)
      */
-    public static final int SC_MULTI_STATUS = 207;
+    int SC_MULTI_STATUS = 207;
 
     // --- 3xx Redirection ---
     /** {@code 3xx Redirection} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_REDIRECTION = 300;
+    int SC_REDIRECTION = 300;
 
     /** {@code 300 Mutliple Choices} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_MULTIPLE_CHOICES = 300;
+    int SC_MULTIPLE_CHOICES = 300;
     /** {@code 301 Moved Permanently} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_MOVED_PERMANENTLY = 301;
+    int SC_MOVED_PERMANENTLY = 301;
     /** {@code 302 Moved Temporarily} (Sometimes {@code Found}) (HTTP/1.0 - RFC 1945) */
-    public static final int SC_MOVED_TEMPORARILY = 302;
+    int SC_MOVED_TEMPORARILY = 302;
     /** {@code 303 See Other} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_SEE_OTHER = 303;
+    int SC_SEE_OTHER = 303;
     /** {@code 304 Not Modified} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_NOT_MODIFIED = 304;
+    int SC_NOT_MODIFIED = 304;
     /** {@code 305 Use Proxy} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_USE_PROXY = 305;
+    int SC_USE_PROXY = 305;
     /** {@code 307 Temporary Redirect} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_TEMPORARY_REDIRECT = 307;
+    int SC_TEMPORARY_REDIRECT = 307;
 
     // --- 4xx Client Error ---
     /** {@code 4xx Client Error} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_CLIENT_ERROR = 400;
+    int SC_CLIENT_ERROR = 400;
 
     /** {@code 400 Bad Request} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_BAD_REQUEST = 400;
+    int SC_BAD_REQUEST = 400;
     /** {@code 401 Unauthorized} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_UNAUTHORIZED = 401;
+    int SC_UNAUTHORIZED = 401;
     /** {@code 402 Payment Required} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_PAYMENT_REQUIRED = 402;
+    int SC_PAYMENT_REQUIRED = 402;
     /** {@code 403 Forbidden} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_FORBIDDEN = 403;
+    int SC_FORBIDDEN = 403;
     /** {@code 404 Not Found} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_NOT_FOUND = 404;
+    int SC_NOT_FOUND = 404;
     /** {@code 405 Method Not Allowed} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_METHOD_NOT_ALLOWED = 405;
+    int SC_METHOD_NOT_ALLOWED = 405;
     /** {@code 406 Not Acceptable} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_NOT_ACCEPTABLE = 406;
+    int SC_NOT_ACCEPTABLE = 406;
     /** {@code 407 Proxy Authentication Required} (HTTP/1.1 - RFC 2616)*/
-    public static final int SC_PROXY_AUTHENTICATION_REQUIRED = 407;
+    int SC_PROXY_AUTHENTICATION_REQUIRED = 407;
     /** {@code 408 Request Timeout} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_REQUEST_TIMEOUT = 408;
+    int SC_REQUEST_TIMEOUT = 408;
     /** {@code 409 Conflict} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_CONFLICT = 409;
+    int SC_CONFLICT = 409;
     /** {@code 410 Gone} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_GONE = 410;
+    int SC_GONE = 410;
     /** {@code 411 Length Required} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_LENGTH_REQUIRED = 411;
+    int SC_LENGTH_REQUIRED = 411;
     /** {@code 412 Precondition Failed} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_PRECONDITION_FAILED = 412;
+    int SC_PRECONDITION_FAILED = 412;
     /** {@code 413 Request Entity Too Large} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_REQUEST_TOO_LONG = 413;
+    int SC_REQUEST_TOO_LONG = 413;
     /** {@code 414 Request-URI Too Long} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_REQUEST_URI_TOO_LONG = 414;
+    int SC_REQUEST_URI_TOO_LONG = 414;
     /** {@code 415 Unsupported Media Type} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_UNSUPPORTED_MEDIA_TYPE = 415;
+    int SC_UNSUPPORTED_MEDIA_TYPE = 415;
     /** {@code 416 Requested Range Not Satisfiable} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
+    int SC_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
     /** {@code 417 Expectation Failed} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_EXPECTATION_FAILED = 417;
+    int SC_EXPECTATION_FAILED = 417;
     /** {@code 421 Misdirected Request} (HTTP/2 - RFC 7540) */
-    public static final int SC_MISDIRECTED_REQUEST = 421;
+    int SC_MISDIRECTED_REQUEST = 421;
 
     /**
      * Static constant for a 418 error.
@@ -142,7 +140,7 @@ public interface HttpStatus {
      * or {@code 418 Reauthentication Required} (HTTP/1.1 drafts?)
      */
     // not used
-    // public static final int SC_UNPROCESSABLE_ENTITY = 418;
+    // int SC_UNPROCESSABLE_ENTITY = 418;
 
     /**
      * Static constant for a 419 error.
@@ -151,38 +149,38 @@ public interface HttpStatus {
      * or {@code 419 Proxy Reauthentication Required}
      * (HTTP/1.1 drafts?)
      */
-    public static final int SC_INSUFFICIENT_SPACE_ON_RESOURCE = 419;
+    int SC_INSUFFICIENT_SPACE_ON_RESOURCE = 419;
     /**
      * Static constant for a 420 error.
      * {@code 420 Method Failure}
      * (WebDAV - draft-ietf-webdav-protocol-05?)
      */
-    public static final int SC_METHOD_FAILURE = 420;
+    int SC_METHOD_FAILURE = 420;
     /** {@code 422 Unprocessable Entity} (WebDAV - RFC 2518) */
-    public static final int SC_UNPROCESSABLE_ENTITY = 422;
+    int SC_UNPROCESSABLE_ENTITY = 422;
     /** {@code 423 Locked} (WebDAV - RFC 2518) */
-    public static final int SC_LOCKED = 423;
+    int SC_LOCKED = 423;
     /** {@code 424 Failed Dependency} (WebDAV - RFC 2518) */
-    public static final int SC_FAILED_DEPENDENCY = 424;
+    int SC_FAILED_DEPENDENCY = 424;
 
     // --- 5xx Server Error ---
     /** {@code 500 Server Error} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_SERVER_ERROR = 500;
+    int SC_SERVER_ERROR = 500;
 
     /** {@code 500 Internal Server Error} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_INTERNAL_SERVER_ERROR = 500;
+    int SC_INTERNAL_SERVER_ERROR = 500;
     /** {@code 501 Not Implemented} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_NOT_IMPLEMENTED = 501;
+    int SC_NOT_IMPLEMENTED = 501;
     /** {@code 502 Bad Gateway} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_BAD_GATEWAY = 502;
+    int SC_BAD_GATEWAY = 502;
     /** {@code 503 Service Unavailable} (HTTP/1.0 - RFC 1945) */
-    public static final int SC_SERVICE_UNAVAILABLE = 503;
+    int SC_SERVICE_UNAVAILABLE = 503;
     /** {@code 504 Gateway Timeout} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_GATEWAY_TIMEOUT = 504;
+    int SC_GATEWAY_TIMEOUT = 504;
     /** {@code 505 HTTP Version Not Supported} (HTTP/1.1 - RFC 2616) */
-    public static final int SC_HTTP_VERSION_NOT_SUPPORTED = 505;
+    int SC_HTTP_VERSION_NOT_SUPPORTED = 505;
 
     /** {@code 507 Insufficient Storage} (WebDAV - RFC 2518) */
-    public static final int SC_INSUFFICIENT_STORAGE = 507;
+    int SC_INSUFFICIENT_STORAGE = 507;
 
 }
