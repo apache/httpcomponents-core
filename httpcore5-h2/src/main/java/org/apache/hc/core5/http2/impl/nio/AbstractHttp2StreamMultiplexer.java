2f01bf902a1821c7eaf877fc18269122520283ca
Oleg Kalnichevski <olegk@apache.org>
2016-12-14 20:19:33 +0000

    Work-around for what appears to be a bug in Ngnix 1.11
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1774336 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore5-h2/src/main/java/org/apache/hc/core5/http2/impl/nio/AbstractHttp2StreamMultiplexer.java
+++ httpcore5-h2/src/main/java/org/apache/hc/core5/http2/impl/nio/AbstractHttp2StreamMultiplexer.java
@@ -171,8 +171,16 @@ abstract class AbstractHttp2StreamMultiplexer implements HttpConnection {
     private int updateWindow(final AtomicInteger window, final int delta) throws ArithmeticException {
         for (;;) {
             final int current = window.get();
-            final long newValue = (long) current + delta;
-            if (Math.abs(newValue) > Integer.MAX_VALUE) {
+            long newValue = (long) current + delta;
+
+            //TODO: work-around for what looks like a bug in Ngnix (1.11)
+            // Tolerate if the update window exceeded by one
+            if (newValue == 0x80000000L) {
+                newValue = Integer.MAX_VALUE;
+            }
+            //TODO: needs to be removed
+
+            if (Math.abs(newValue) > 0x7fffffffL) {
                 throw new ArithmeticException("Update causes flow control window to exceed " + Integer.MAX_VALUE);
             }
             if (window.compareAndSet(current, (int) newValue)) {
