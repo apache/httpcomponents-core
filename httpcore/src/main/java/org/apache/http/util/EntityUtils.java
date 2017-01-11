f484626c086537409e8091ba9bd521e503ae4119
Gary D. Gregory <ggregory@apache.org>
2017-01-11 23:45:05 +0000

    Better local var name.
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/branches/4.4.x@1778358 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore/src/main/java/org/apache/http/util/EntityUtils.java
+++ httpcore/src/main/java/org/apache/http/util/EntityUtils.java
@@ -128,11 +128,11 @@ public final class EntityUtils {
         try {
             Args.check(entity.getContentLength() <= Integer.MAX_VALUE,
                     "HTTP entity too large to be buffered in memory");
-            int i = (int)entity.getContentLength();
-            if (i < 0) {
-                i = DEFAULT_BUFFER_SIZE;
+            int capacity = (int)entity.getContentLength();
+            if (capacity < 0) {
+                capacity = DEFAULT_BUFFER_SIZE;
             }
-            final ByteArrayBuffer buffer = new ByteArrayBuffer(i);
+            final ByteArrayBuffer buffer = new ByteArrayBuffer(capacity);
             final byte[] tmp = new byte[DEFAULT_BUFFER_SIZE];
             int l;
             while((l = instream.read(tmp)) != -1) {
@@ -205,9 +205,9 @@ public final class EntityUtils {
         try {
             Args.check(entity.getContentLength() <= Integer.MAX_VALUE,
                     "HTTP entity too large to be buffered in memory");
-            int i = (int)entity.getContentLength();
-            if (i < 0) {
-                i = DEFAULT_BUFFER_SIZE;
+            int capacity = (int)entity.getContentLength();
+            if (capacity < 0) {
+                capacity = DEFAULT_BUFFER_SIZE;
             }
             Charset charset = null;
             if (contentType != null) {
@@ -221,7 +221,7 @@ public final class EntityUtils {
                 charset = HTTP.DEF_CONTENT_CHARSET;
             }
             final Reader reader = new InputStreamReader(instream, charset);
-            final CharArrayBuffer buffer = new CharArrayBuffer(i);
+            final CharArrayBuffer buffer = new CharArrayBuffer(capacity);
             final char[] tmp = new char[1024];
             int l;
             while((l = reader.read(tmp)) != -1) {
