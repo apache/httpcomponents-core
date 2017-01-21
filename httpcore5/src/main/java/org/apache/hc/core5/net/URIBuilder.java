10ffb85a335e01efbb701dd97e3758bd35239f07
Oleg Kalnichevski <olegk@apache.org>
2017-01-21 14:43:00 +0000

    HTTPCLIENT-1803: Improved handling of malformed paths by URIBuilder
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1779734 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore5/src/main/java/org/apache/hc/core5/net/URIBuilder.java
+++ httpcore5/src/main/java/org/apache/hc/core5/net/URIBuilder.java
@@ -474,7 +474,7 @@ public class URIBuilder {
     private static String normalizePath(final String path) {
         String s = path;
         if (s == null) {
-            return null;
+            return "/";
         }
         int n = 0;
         for (; n < s.length(); n++) {
@@ -484,6 +484,9 @@ public class URIBuilder {
         }
         if (n > 1) {
             s = s.substring(n - 1);
+        }
+        if (!s.startsWith("/")) {
+            s = "/" + s;
         }
         return s;
     }
