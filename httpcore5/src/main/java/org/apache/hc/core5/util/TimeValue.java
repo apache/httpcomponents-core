adf940fe542154a40145a110b434eb47a73ff13d
Gary D. Gregory <ggregory@apache.org>
2017-03-31 18:44:57 +0000

    New TimeValue class. Using it will be next.
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1789723 13f79535-47bb-0310-9956-ffa450edef68

---
+++ httpcore5/src/main/java/org/apache/hc/core5/util/TimeValue.java
@@ -0,0 +1,105 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.hc.core5.util;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Wraps a time (long) and TimeUnit.
+ * 
+ * @since 5.0
+ */
+public class TimeValue {
+
+    private final long duration;
+
+    private final TimeUnit timeUnit;
+
+    public TimeValue(long duration, TimeUnit timeUnit) {
+        super();
+        this.duration = duration;
+        this.timeUnit = timeUnit;
+    }
+
+    public long convert(TimeUnit sourceUnit) {
+        return timeUnit.convert(duration, sourceUnit);
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public TimeUnit getTimeUnit() {
+        return timeUnit;
+    }
+
+    public void sleep() throws InterruptedException {
+        timeUnit.sleep(duration);
+    }
+
+    public void timedJoin(Thread thread) throws InterruptedException {
+        timeUnit.timedJoin(thread, duration);
+    }
+
+    public void timedWait(Object obj) throws InterruptedException {
+        timeUnit.timedWait(obj, duration);
+    }
+
+    public long toDays() {
+        return timeUnit.toDays(duration);
+    }
+
+    public long toHours() {
+        return timeUnit.toHours(duration);
+    }
+
+    public long toMicros() {
+        return timeUnit.toMicros(duration);
+    }
+
+    public long toMillis() {
+        return timeUnit.toMillis(duration);
+    }
+
+    public long toMinutes() {
+        return timeUnit.toMinutes(duration);
+    }
+
+    public long toNanos() {
+        return timeUnit.toNanos(duration);
+    }
+
+    public long toSeconds() {
+        return timeUnit.toSeconds(duration);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%,d %s", Long.valueOf(duration), timeUnit);
+    }
+}
