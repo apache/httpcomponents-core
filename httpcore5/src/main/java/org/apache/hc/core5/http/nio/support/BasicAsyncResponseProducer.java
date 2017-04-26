fe88c7b31bbde15853257402dffbeff7c117a2ff
Oleg Kalnichevski <olegk@apache.org>
2017-04-26 19:20:57 +0000

    Added some basic convenience request/response producers/consumers
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1792795 13f79535-47bb-0310-9956-ffa450edef68

---
+++ httpcore5/src/main/java/org/apache/hc/core5/http/nio/support/BasicAsyncResponseProducer.java
@@ -0,0 +1,83 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.hc.core5.http.nio.support;
+
+import java.io.IOException;
+
+import org.apache.hc.core5.http.EntityDetails;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.nio.AsyncEntityProducer;
+import org.apache.hc.core5.http.nio.AsyncResponseProducer;
+import org.apache.hc.core5.http.nio.DataStreamChannel;
+import org.apache.hc.core5.util.Args;
+
+public class BasicAsyncResponseProducer implements AsyncResponseProducer {
+
+    private final HttpResponse response;
+    private final AsyncEntityProducer entityProducer;
+
+    public BasicAsyncResponseProducer(final HttpResponse response, final AsyncEntityProducer entityProducer) {
+        Args.notNull(response, "Response");
+        this.response = response;
+        this.entityProducer = entityProducer;
+    }
+
+    @Override
+    public HttpResponse produceResponse() {
+        return response;
+    }
+
+    @Override
+    public final EntityDetails getEntityDetails() {
+        return entityProducer;
+    }
+
+    @Override
+    public final int available() {
+        return entityProducer != null ? entityProducer.available() : 0;
+    }
+
+    @Override
+    public final void produce(final DataStreamChannel channel) throws IOException {
+        if (entityProducer != null) {
+            entityProducer.produce(channel);
+        }
+    }
+
+    @Override
+    public final void failed(final Exception cause) {
+        releaseResources();
+    }
+
+    @Override
+    public final void releaseResources() {
+        if (entityProducer != null) {
+            entityProducer.releaseResources();
+        }
+    }
+
+}
