3ee0f67c985720debb1891b044510c015ec513ea
Gary D. Gregory <ggregory@apache.org>
2017-05-01 18:45:11 +0000

    Add test.
    git-svn-id: https://svn.apache.org/repos/asf/httpcomponents/httpcore/trunk@1793397 13f79535-47bb-0310-9956-ffa450edef68

--- httpcore5/src/test/java/org/apache/hc/core5/util/TestTimeValue.java
+++ httpcore5/src/test/java/org/apache/hc/core5/util/TestTimeValue.java
@@ -138,4 +138,10 @@ public class TestTimeValue {
         test(-1);
     }
 
+    @Test
+    public void testToString() {
+        Assert.assertEquals("9,223,372,036,854,775,807 SECONDS", TimeValue.ofSeconds(Long.MAX_VALUE).toString());
+        Assert.assertEquals("0 MILLISECONDS", TimeValue.ZERO_MILLISECONDS.toString());
+    }
+
 }
