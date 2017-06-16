e5f75c9cca97260118dfee197eb0f4158b140482
Oleg Kalnichevski <olegk@apache.org>
2017-06-16 11:35:53 +0200

    Fixed SingleCoreIOReactor not catching CancelledKeyException when handling events

--- httpcore5/src/main/java/org/apache/hc/core5/reactor/SingleCoreIOReactor.java
+++ httpcore5/src/main/java/org/apache/hc/core5/reactor/SingleCoreIOReactor.java
@@ -45,6 +45,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import org.apache.hc.core5.concurrent.ComplexFuture;
 import org.apache.hc.core5.concurrent.FutureCallback;
 import org.apache.hc.core5.function.Callback;
+import org.apache.hc.core5.io.ShutdownType;
 import org.apache.hc.core5.net.NamedEndpoint;
 import org.apache.hc.core5.util.Args;
 import org.apache.hc.core5.util.TimeValue;
@@ -160,7 +161,11 @@ class SingleCoreIOReactor extends AbstractSingleCoreIOReactor implements Connect
     private void processEvents(final Set<SelectionKey> selectedKeys) {
         for (final SelectionKey key : selectedKeys) {
             final InternalChannel channel = (InternalChannel) key.attachment();
-            channel.handleIOEvent(key.readyOps());
+            try {
+                channel.handleIOEvent(key.readyOps());
+            } catch (final CancelledKeyException ex) {
+                channel.shutdown(ShutdownType.GRACEFUL);
+            }
         }
         selectedKeys.clear();
     }
