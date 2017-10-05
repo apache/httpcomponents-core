d07a2a78466e3ccf2e4054c4306d55c0e3d896a8
Gary Gregory <garydgregory@gmail.com>
2017-10-05 11:48:49 -0600

    [HTTPCORE-494] Add image constants to ContentType.

--- httpcore5/src/main/java/org/apache/hc/core5/http/ContentType.java
+++ httpcore5/src/main/java/org/apache/hc/core5/http/ContentType.java
@@ -78,6 +78,16 @@ public final class ContentType implements Serializable {
             "application/xhtml+xml", StandardCharsets.ISO_8859_1);
     public static final ContentType APPLICATION_XML = create(
             "application/xml", StandardCharsets.ISO_8859_1);
+    public static final ContentType IMAGE_BMP = create(
+            "image/bmp");
+    public static final ContentType IMAGE_GIF= create(
+            "image/gif");
+    public static final ContentType IMAGE_JPEG = create(
+            "image/jpeg");
+    public static final ContentType IMAGE_PNG = create(
+            "image/png");
+    public static final ContentType IMAGE_WEBP = create(
+            "image/webp");
     public static final ContentType MULTIPART_FORM_DATA = create(
             "multipart/form-data", StandardCharsets.ISO_8859_1);
     public static final ContentType TEXT_HTML = create(
@@ -100,6 +110,11 @@ public final class ContentType implements Serializable {
             APPLICATION_SVG_XML,
             APPLICATION_XHTML_XML,
             APPLICATION_XML,
+            IMAGE_BMP,
+            IMAGE_GIF,
+            IMAGE_JPEG,
+            IMAGE_PNG,
+            IMAGE_WEBP,
             MULTIPART_FORM_DATA,
             TEXT_HTML,
             TEXT_PLAIN,
