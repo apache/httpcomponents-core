1c4729b04745d47d2ff8fc706df9f1e454521170
Gary Gregory <ggregory@apache.org>
2017-12-12 09:18:02 -0700

    Better Javadoc for NameValuePair.

--- httpcore5/src/main/java/org/apache/hc/core5/http/NameValuePair.java
+++ httpcore5/src/main/java/org/apache/hc/core5/http/NameValuePair.java
@@ -28,14 +28,24 @@
 package org.apache.hc.core5.http;
 
 /**
- * A name / value pair parameter used as an element of HTTP messages.
+ * A name-value pair parameter used as an element of HTTP messages.
  *
  * @since 4.0
  */
 public interface NameValuePair {
 
+    /**
+     * Gets the name of this pair.
+     *
+     * @return the name of this pair, never {@code null}.
+     */
     String getName();
 
+    /**
+     * Gets the value of this pair.
+     *
+     * @return the value of this pair, may be {@code null}.
+     */
     String getValue();
 
 }
