694db38c1ec148a0d8e2f37aa07f3f6f2089a5d2
Oleg Kalnichevski <olegk@apache.org>
2017-12-18 12:03:34 +0100

    Added ComplexCancellable that represents a sequence of Cancellable processes

---
+++ httpcore5/src/main/java/org/apache/hc/core5/concurrent/ComplexCancellable.java
@@ -0,0 +1,78 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.hc.core5.concurrent;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.apache.hc.core5.util.Args;
+
+/**
+ * Cancellable that represents a sequence of other {@link Cancellable} processes.
+ * Dependent process will get cancelled if the future itself is cancelled.
+ *
+ * @since 5.0
+ */
+public final class ComplexCancellable implements Cancellable, CancellableDependency {
+
+    private final AtomicReference<Cancellable> dependencyRef;
+    private final AtomicBoolean cancelled;
+
+    public ComplexCancellable() {
+        this.dependencyRef = new AtomicReference<>(null);
+        this.cancelled = new AtomicBoolean(false);
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled.get();
+    }
+
+    @Override
+    public void setDependency(final Cancellable dependency) {
+        Args.notNull(dependency, "dependency");
+        if (!cancelled.get()) {
+            dependencyRef.set(dependency);
+        } else {
+            dependency.cancel();
+        }
+    }
+
+    @Override
+    public boolean cancel() {
+        if (cancelled.compareAndSet(false, true)) {
+            final Cancellable dependency = dependencyRef.getAndSet(null);
+            if (dependency != null) {
+                dependency.cancel();
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ httpcore5/src/test/java/org/apache/hc/core5/concurrent/TestComplexCancellable.java
@@ -0,0 +1,53 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+package org.apache.hc.core5.concurrent;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestComplexCancellable {
+
+    @Test
+    public void testCancelled() throws Exception {
+        final ComplexCancellable cancellable = new ComplexCancellable();
+
+        final BasicFuture<Object> dependency1 = new BasicFuture<>(null);
+        cancellable.setDependency(dependency1);
+
+        Assert.assertFalse(cancellable.isCancelled());
+
+        cancellable.cancel();
+        Assert.assertThat(cancellable.isCancelled(), CoreMatchers.is(true));
+        Assert.assertThat(dependency1.isCancelled(), CoreMatchers.is(true));
+
+        final BasicFuture<Object> dependency2 = new BasicFuture<>(null);
+        cancellable.setDependency(dependency2);
+        Assert.assertThat(dependency2.isCancelled(), CoreMatchers.is(true));
+    }
+
+}
