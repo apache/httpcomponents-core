ec2ee3d20bba3e9379eca0c4ec4bb5f5a4ca1775
Gary Gregory <ggregory@apache.org>
2018-08-05 11:02:26 -0600

    Fix Javaodc.

--- httpcore5/src/main/java/org/apache/hc/core5/http/ConnectionClosedException.java
+++ httpcore5/src/main/java/org/apache/hc/core5/http/ConnectionClosedException.java
@@ -39,7 +39,7 @@ public class ConnectionClosedException extends IOException {
     private static final long serialVersionUID = 617550366255636674L;
 
     /**
-     * Creates a new ConnectionClosedException with the specified detail message.
+     * Creates a new ConnectionClosedException with a default message.
      */
     public ConnectionClosedException() {
         super("Connection is closed");
