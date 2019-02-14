d83eeb43f133f162de9dca9ab2207df27de6394a
Oleg Kalnichevski <olegk@apache.org>
2019-02-14 13:31:58 +0100

    Added EntityTemplate class similar to that provided in 4.4

---
+++ httpcore5/src/main/java/org/apache/hc/core5/http/io/entity/EntityTemplate.java
@@ -0,0 +1,92 @@
+/*
+ * ====================================================================
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Apache Software Foundation.  For more
+ * information on the Apache Software Foundation, please see
+ * <http://www.apache.org/>.
+ *
+ */
+
+package org.apache.hc.core5.http.io.entity;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.apache.hc.core5.annotation.Contract;
+import org.apache.hc.core5.annotation.ThreadingBehavior;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.util.Args;
+
+/**
+ * Entity that delegates the process of content generation to a {@link HttpContentProducer}.
+ *
+ * @since 4.0
+ */
+@Contract(threading = ThreadingBehavior.IMMUTABLE_CONDITIONAL)
+public final class EntityTemplate extends AbstractHttpEntity {
+
+    private final long contentLength;
+    private final HttpContentProducer contentProducer;
+
+    public EntityTemplate(
+            final long contentLength, final ContentType contentType, final String contentEncoding,
+            final HttpContentProducer contentProducer) {
+        super(contentType, contentEncoding);
+        this.contentLength = contentLength;
+        this.contentProducer = Args.notNull(contentProducer, "Content producer");
+    }
+
+    @Override
+    public long getContentLength() {
+        return contentLength;
+    }
+
+    @Override
+    public InputStream getContent() throws IOException {
+        final ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        writeTo(buf);
+        return new ByteArrayInputStream(buf.toByteArray());
+    }
+
+    @Override
+    public boolean isRepeatable() {
+        return true;
+    }
+
+    @Override
+    public void writeTo(final OutputStream outStream) throws IOException {
+        Args.notNull(outStream, "Output stream");
+        this.contentProducer.writeTo(outStream);
+    }
+
+    @Override
+    public boolean isStreaming() {
+        return false;
+    }
+
+    @Override
+    public void close() throws IOException {
+    }
+
+}
