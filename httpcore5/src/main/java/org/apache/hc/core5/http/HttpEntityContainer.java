589b38c1d076c396ac98179a9a8ec86413b7dcfc
Gary Gregory <ggregory@rocketsoftware.com>
2019-04-21 21:11:59 -0400

    Refactor ClassicHttpRequest and ClassicHttpResponse interfaces to extend a new interface called HttpEntityContainer. This permits for writting comon entity code for both requests and responses without testing and casting for two cases.

--- httpcore5/src/main/java/org/apache/hc/core5/http/HttpEntityContainer.java
+++ httpcore5/src/main/java/org/apache/hc/core5/http/HttpEntityContainer.java
@@ -27,13 +27,29 @@
 
 package org.apache.hc.core5.http;
 
-import java.io.Closeable;
-
 /**
- * 'Classic' {@link HttpResponse} message that can enclose {@link HttpEntity}.
+ * Contains an {@link HttpEntity}.
  *
  * @since 5.0
  */
-public interface ClassicHttpResponse extends HttpResponse, HttpEntityContainer, Closeable {
-    // empty
+public interface HttpEntityContainer {
+
+    /**
+     * Obtains the message entity, if available.
+     *
+     * @return  the message entity, or {@code null} if not available
+     */
+    HttpEntity getEntity();
+
+    /**
+     * Sets an entity for this message.
+     * <p>
+     * Please note that if an entity has already been set it is responsibility of the caller
+     * to ensure release of the resources that may be associated with that entity.
+     * </p>
+     *
+     * @param entity    the entity to set of this message, or {@code null} to unset
+     */
+    void setEntity(HttpEntity entity);
+
 }
