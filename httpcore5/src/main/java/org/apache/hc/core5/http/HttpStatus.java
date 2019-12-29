a436caa2162f3f3d1e598e90a20721a5f4f4dc0b
Oleg Kalnichevski <olegk@apache.org>
2019-12-29 14:14:53 +0100

    HTTPCORE-617: removed references to specific RFC documents where irrelevant; removed references to RFC documents superseded by newer ones

--- httpcore5/src/main/java/org/apache/hc/core5/http/HttpStatus.java
+++ httpcore5/src/main/java/org/apache/hc/core5/http/HttpStatus.java
@@ -29,32 +29,30 @@ package org.apache.hc.core5.http;
 
 /**
  * Constants enumerating the HTTP status codes.
- * All status codes defined in RFC1945 (HTTP/1.0), RFC2616 (HTTP/1.1),
- * RFC2518 (WebDAV), RFC7540 (HTTP/2), RFC6585 (Additional HTTP Status Codes)
- * RFC8297 (Early Hints), RFC7538 (Permanent Redirect), RFC7725 (An HTTP Status
- * Code to Report Legal Obstacles) and RFC2295 (Transparent Content Negotiation)
- * are listed.
+ * All status codes defined in RFC 7231 (HTTP/1.1), RFC 2518 (WebDAV), RFC 7540 (HTTP/2),
+ * RFC 6585 (Additional HTTP Status Codes), RFC 8297 (Early Hints), RFC 7538 (Permanent Redirect),
+ * RFC 7725 (An HTTP Status Code to Report Legal Obstacles) and RFC 2295 (Transparent Content
+ * Negotiation) are listed.
  *
- * @see <a href="https://tools.ietf.org/html/rfc1945">RFC1945 (HTTP/1.0)</a>
- * @see <a href="https://tools.ietf.org/html/rfc2616">RFC2616 (HTTP/1.1)</a>
- * @see <a href="https://tools.ietf.org/html/rfc2518">RFC2518 (WebDAV)</a>
- * @see <a href="https://tools.ietf.org/html/rfc7540">RFC7540 (HTTP/2)</a>
- * @see <a href="https://tools.ietf.org/html/rfc6585">RFC6585 (Additional HTTP Status Codes)</a>
- * @see <a href="https://tools.ietf.org/html/rfc8297">RFC8297 (Early Hints)</a>
- * @see <a href="https://tools.ietf.org/html/rfc7538">RFC7538 (Permanent Redirect)</a>
- * @see <a href="https://tools.ietf.org/html/rfc7725">RFC7725 (An HTTP Status Code to Report Legal Obstacles)</a>
- * @see <a href="https://tools.ietf.org/html/rfc2295">RFC2295 (Transparent Content Negotiation)</a>
+ * @see <a href="https://tools.ietf.org/html/rfc7231">RFC 7231 (HTTP/1.1)</a>
+ * @see <a href="https://tools.ietf.org/html/rfc2518">RFC 2518 (WebDAV)</a>
+ * @see <a href="https://tools.ietf.org/html/rfc7540">RFC 7540 (HTTP/2)</a>
+ * @see <a href="https://tools.ietf.org/html/rfc6585">RFC 6585 (Additional HTTP Status Codes)</a>
+ * @see <a href="https://tools.ietf.org/html/rfc8297">RFC 8297 (Early Hints)</a>
+ * @see <a href="https://tools.ietf.org/html/rfc7538">RFC 7538 (Permanent Redirect)</a>
+ * @see <a href="https://tools.ietf.org/html/rfc7725">RFC 7725 (An HTTP Status Code to Report Legal Obstacles)</a>
+ * @see <a href="https://tools.ietf.org/html/rfc2295">RFC 2295 (Transparent Content Negotiation)</a>
  * @since 4.0
  */
 public interface HttpStatus {
 
     // --- 1xx Informational ---
-    /** {@code 100 1xx Informational} (HTTP/1.1 - RFC 2616) */
+    /** {@code 100 1xx Informational} (HTTP/1.1 - RFC 7231) */
     int SC_INFORMATIONAL = 100;
 
-    /** {@code 100 Continue} (HTTP/1.1 - RFC 2616) */
+    /** {@code 100 Continue} (HTTP/1.1 - RFC 7231) */
     int SC_CONTINUE = 100;
-    /** {@code 101 Switching Protocols} (HTTP/1.1 - RFC 2616)*/
+    /** {@code 101 Switching Protocols} (HTTP/1.1 - RFC 7231)*/
     int SC_SWITCHING_PROTOCOLS = 101;
     /** {@code 102 Processing} (WebDAV - RFC 2518) */
     int SC_PROCESSING = 102;
@@ -62,22 +60,22 @@ public interface HttpStatus {
     int SC_EARLY_HINTS = 103;
 
     // --- 2xx Success ---
-    /** {@code 2xx Success} (HTTP/1.0 - RFC 1945) */
+    /** {@code 2xx Success} (HTTP/1.0 - RFC 7231) */
     int SC_SUCCESS = 200;
 
-    /** {@code 200 OK} (HTTP/1.0 - RFC 1945) */
+    /** {@code 200 OK} (HTTP/1.0 - RFC 7231) */
     int SC_OK = 200;
-    /** {@code 201 Created} (HTTP/1.0 - RFC 1945) */
+    /** {@code 201 Created} (HTTP/1.0 - RFC 7231) */
     int SC_CREATED = 201;
-    /** {@code 202 Accepted} (HTTP/1.0 - RFC 1945) */
+    /** {@code 202 Accepted} (HTTP/1.0 - RFC 7231) */
     int SC_ACCEPTED = 202;
-    /** {@code 203 Non Authoritative Information} (HTTP/1.1 - RFC 2616) */
+    /** {@code 203 Non Authoritative Information} (HTTP/1.1 - RFC 7231) */
     int SC_NON_AUTHORITATIVE_INFORMATION = 203;
-    /** {@code 204 No Content} (HTTP/1.0 - RFC 1945) */
+    /** {@code 204 No Content} (HTTP/1.0 - RFC 7231) */
     int SC_NO_CONTENT = 204;
-    /** {@code 205 Reset Content} (HTTP/1.1 - RFC 2616) */
+    /** {@code 205 Reset Content} (HTTP/1.1 - RFC 7231) */
     int SC_RESET_CONTENT = 205;
-    /** {@code 206 Partial Content} (HTTP/1.1 - RFC 2616) */
+    /** {@code 206 Partial Content} (HTTP/1.1 - RFC 7231) */
     int SC_PARTIAL_CONTENT = 206;
     /**
      * {@code 207 Multi-Status} (WebDAV - RFC 2518)
@@ -95,77 +93,69 @@ public interface HttpStatus {
     int SC_IM_USED = 226;
 
     // --- 3xx Redirection ---
-    /** {@code 3xx Redirection} (HTTP/1.1 - RFC 2616) */
+    /** {@code 3xx Redirection} (HTTP/1.1 - RFC 7231) */
     int SC_REDIRECTION = 300;
 
-    /** {@code 300 Mutliple Choices} (HTTP/1.1 - RFC 2616) */
+    /** {@code 300 Mutliple Choices} (HTTP/1.1 - RFC 7231) */
     int SC_MULTIPLE_CHOICES = 300;
-    /** {@code 301 Moved Permanently} (HTTP/1.0 - RFC 1945) */
+    /** {@code 301 Moved Permanently} (HTTP/1.0 - RFC 7231) */
     int SC_MOVED_PERMANENTLY = 301;
-    /** {@code 302 Moved Temporarily} (Sometimes {@code Found}) (HTTP/1.0 - RFC 1945) */
+    /** {@code 302 Moved Temporarily} (Sometimes {@code Found}) (HTTP/1.0 - RFC 7231) */
     int SC_MOVED_TEMPORARILY = 302;
-    /** {@code 303 See Other} (HTTP/1.1 - RFC 2616) */
+    /** {@code 303 See Other} (HTTP/1.1 - RFC 7231) */
     int SC_SEE_OTHER = 303;
-    /** {@code 304 Not Modified} (HTTP/1.0 - RFC 1945) */
+    /** {@code 304 Not Modified} (HTTP/1.0 - RFC 7231) */
     int SC_NOT_MODIFIED = 304;
-    /** {@code 305 Use Proxy} (HTTP/1.1 - RFC 2616) */
+    /** {@code 305 Use Proxy} (HTTP/1.1 - RFC 7231) */
     int SC_USE_PROXY = 305;
-    /** {@code 307 Temporary Redirect} (HTTP/1.1 - RFC 2616) */
+    /** {@code 307 Temporary Redirect} (HTTP/1.1 - RFC 7231) */
     int SC_TEMPORARY_REDIRECT = 307;
 
     /** {@code 308 Permanent Redirect} (HTTP/1.1 - RFC 7538) */
     int SC_PERMANENT_REDIRECT = 308;
 
     // --- 4xx Client Error ---
-    /** {@code 4xx Client Error} (HTTP/1.1 - RFC 2616) */
+    /** {@code 4xx Client Error} (HTTP/1.1 - RFC 7231) */
     int SC_CLIENT_ERROR = 400;
 
-    /** {@code 400 Bad Request} (HTTP/1.1 - RFC 2616) */
+    /** {@code 400 Bad Request} (HTTP/1.1 - RFC 7231) */
     int SC_BAD_REQUEST = 400;
-    /** {@code 401 Unauthorized} (HTTP/1.0 - RFC 1945) */
+    /** {@code 401 Unauthorized} (HTTP/1.0 - RFC 7231) */
     int SC_UNAUTHORIZED = 401;
-    /** {@code 402 Payment Required} (HTTP/1.1 - RFC 2616) */
+    /** {@code 402 Payment Required} (HTTP/1.1 - RFC 7231) */
     int SC_PAYMENT_REQUIRED = 402;
-    /** {@code 403 Forbidden} (HTTP/1.0 - RFC 1945) */
+    /** {@code 403 Forbidden} (HTTP/1.0 - RFC 7231) */
     int SC_FORBIDDEN = 403;
-    /** {@code 404 Not Found} (HTTP/1.0 - RFC 1945) */
+    /** {@code 404 Not Found} (HTTP/1.0 - RFC 7231) */
     int SC_NOT_FOUND = 404;
-    /** {@code 405 Method Not Allowed} (HTTP/1.1 - RFC 2616) */
+    /** {@code 405 Method Not Allowed} (HTTP/1.1 - RFC 7231) */
     int SC_METHOD_NOT_ALLOWED = 405;
-    /** {@code 406 Not Acceptable} (HTTP/1.1 - RFC 2616) */
+    /** {@code 406 Not Acceptable} (HTTP/1.1 - RFC 7231) */
     int SC_NOT_ACCEPTABLE = 406;
-    /** {@code 407 Proxy Authentication Required} (HTTP/1.1 - RFC 2616)*/
+    /** {@code 407 Proxy Authentication Required} (HTTP/1.1 - RFC 7231)*/
     int SC_PROXY_AUTHENTICATION_REQUIRED = 407;
-    /** {@code 408 Request Timeout} (HTTP/1.1 - RFC 2616) */
+    /** {@code 408 Request Timeout} (HTTP/1.1 - RFC 7231) */
     int SC_REQUEST_TIMEOUT = 408;
-    /** {@code 409 Conflict} (HTTP/1.1 - RFC 2616) */
+    /** {@code 409 Conflict} (HTTP/1.1 - RFC 7231) */
     int SC_CONFLICT = 409;
-    /** {@code 410 Gone} (HTTP/1.1 - RFC 2616) */
+    /** {@code 410 Gone} (HTTP/1.1 - RFC 7231) */
     int SC_GONE = 410;
-    /** {@code 411 Length Required} (HTTP/1.1 - RFC 2616) */
+    /** {@code 411 Length Required} (HTTP/1.1 - RFC 7231) */
     int SC_LENGTH_REQUIRED = 411;
-    /** {@code 412 Precondition Failed} (HTTP/1.1 - RFC 2616) */
+    /** {@code 412 Precondition Failed} (HTTP/1.1 - RFC 7231) */
     int SC_PRECONDITION_FAILED = 412;
-    /** {@code 413 Request Entity Too Large} (HTTP/1.1 - RFC 2616) */
+    /** {@code 413 Request Entity Too Large} (HTTP/1.1 - RFC 7231) */
     int SC_REQUEST_TOO_LONG = 413;
-    /** {@code 414 Request-URI Too Long} (HTTP/1.1 - RFC 2616) */
+    /** {@code 414 Request-URI Too Long} (HTTP/1.1 - RFC 7231) */
     int SC_REQUEST_URI_TOO_LONG = 414;
-    /** {@code 415 Unsupported Media Type} (HTTP/1.1 - RFC 2616) */
+    /** {@code 415 Unsupported Media Type} (HTTP/1.1 - RFC 7231) */
     int SC_UNSUPPORTED_MEDIA_TYPE = 415;
-    /** {@code 416 Requested Range Not Satisfiable} (HTTP/1.1 - RFC 2616) */
+    /** {@code 416 Requested Range Not Satisfiable} (HTTP/1.1 - RFC 7231) */
     int SC_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
-    /** {@code 417 Expectation Failed} (HTTP/1.1 - RFC 2616) */
+    /** {@code 417 Expectation Failed} (HTTP/1.1 - RFC 7231) */
     int SC_EXPECTATION_FAILED = 417;
     /** {@code 421 Misdirected Request} (HTTP/2 - RFC 7540) */
     int SC_MISDIRECTED_REQUEST = 421;
-
-    /**
-     * Static constant for a 418 error.
-     * {@code 418 Unprocessable Entity} (WebDAV drafts?)
-     * or {@code 418 Reauthentication Required} (HTTP/1.1 drafts?)
-     */
-    // not used
-    // int SC_UNPROCESSABLE_ENTITY = 418;
 
     /**
      * Static constant for a 419 error.
@@ -197,22 +187,22 @@ public interface HttpStatus {
     int SC_UNAVAILABLE_FOR_LEGAL_REASONS = 451;
 
     // --- 5xx Server Error ---
-    /** {@code 500 Server Error} (HTTP/1.0 - RFC 1945) */
+    /** {@code 500 Server Error} (HTTP/1.0 - RFC 7231) */
     int SC_SERVER_ERROR = 500;
 
-    /** {@code 500 Internal Server Error} (HTTP/1.0 - RFC 1945) */
+    /** {@code 500 Internal Server Error} (HTTP/1.0 - RFC 7231) */
     int SC_INTERNAL_SERVER_ERROR = 500;
-    /** {@code 501 Not Implemented} (HTTP/1.0 - RFC 1945) */
+    /** {@code 501 Not Implemented} (HTTP/1.0 - RFC 7231) */
     int SC_NOT_IMPLEMENTED = 501;
-    /** {@code 502 Bad Gateway} (HTTP/1.0 - RFC 1945) */
+    /** {@code 502 Bad Gateway} (HTTP/1.0 - RFC 7231) */
     int SC_BAD_GATEWAY = 502;
-    /** {@code 503 Service Unavailable} (HTTP/1.0 - RFC 1945) */
+    /** {@code 503 Service Unavailable} (HTTP/1.0 - RFC 7231) */
     int SC_SERVICE_UNAVAILABLE = 503;
-    /** {@code 504 Gateway Timeout} (HTTP/1.1 - RFC 2616) */
+    /** {@code 504 Gateway Timeout} (HTTP/1.1 - RFC 7231) */
     int SC_GATEWAY_TIMEOUT = 504;
-    /** {@code 505 HTTP Version Not Supported} (HTTP/1.1 - RFC 2616) */
+    /** {@code 505 HTTP Version Not Supported} (HTTP/1.1 - RFC 7231) */
     int SC_HTTP_VERSION_NOT_SUPPORTED = 505;
-    /** {@code 506 Variant Also Negotiates} ( Transparent Content Negotiation - RFC2295) */
+    /** {@code 506 Variant Also Negotiates} ( Transparent Content Negotiation - RFC 2295) */
     int SC_VARIANT_ALSO_NEGOTIATES = 506;
     /** {@code 507 Insufficient Storage} (WebDAV - RFC 2518) */
     int SC_INSUFFICIENT_STORAGE = 507;
