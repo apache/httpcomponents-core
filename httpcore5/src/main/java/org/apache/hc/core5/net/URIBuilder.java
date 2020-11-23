d3ce495787892f64b161a19a61eb96ac83ed5e9b
Gary Gregory <garydgregory@gmail.com>
2020-11-23 12:06:09 -0500

    Use Collections.addAll() API instead of loops.

--- httpcore5/src/main/java/org/apache/hc/core5/net/URIBuilder.java
+++ httpcore5/src/main/java/org/apache/hc/core5/net/URIBuilder.java
@@ -630,9 +630,7 @@ public class URIBuilder {
         } else {
             this.queryParams.clear();
         }
-        for (final NameValuePair nvp: nvps) {
-            this.queryParams.add(nvp);
-        }
+        Collections.addAll(this.queryParams, nvps);
         this.encodedQuery = null;
         this.encodedSchemeSpecificPart = null;
         this.query = null;
