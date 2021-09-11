8e37c1c2156432029f737c8ec6a50289cb105277
Oleg Kalnichevski <olegk@apache.org>
2021-09-11 13:52:38 +0200

    HTTPCLIENT-2174: URUBuilder to return a new empty list instead of unmodifiable Collections#emptyList

--- httpcore5/src/main/java/org/apache/hc/core5/net/URIBuilder.java
+++ httpcore5/src/main/java/org/apache/hc/core5/net/URIBuilder.java
@@ -811,7 +811,7 @@ public class URIBuilder {
     }
 
     public List<String> getPathSegments() {
-        return this.pathSegments != null ? new ArrayList<>(this.pathSegments) : Collections.<String>emptyList();
+        return this.pathSegments != null ? new ArrayList<>(this.pathSegments) : new ArrayList<String>();
     }
 
     public String getPath() {
@@ -830,7 +830,7 @@ public class URIBuilder {
     }
 
     public List<NameValuePair> getQueryParams() {
-        return this.queryParams != null ? new ArrayList<>(this.queryParams) : Collections.<NameValuePair>emptyList();
+        return this.queryParams != null ? new ArrayList<>(this.queryParams) : new ArrayList<NameValuePair>();
     }
 
     public String getFragment() {
