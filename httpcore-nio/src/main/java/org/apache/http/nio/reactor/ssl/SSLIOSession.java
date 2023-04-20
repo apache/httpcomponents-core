506f213b4a71aa8fa818afa7b15de6a1cec79f71
Arunan Sugunakumar <arunans23@gmail.com>
2023-04-20 19:51:06 +0530

    HTTPCORE-740: Introduce Read interest during connection close for graceful closure

--- httpcore-nio/src/main/java/org/apache/http/nio/reactor/ssl/SSLIOSession.java
+++ httpcore-nio/src/main/java/org/apache/http/nio/reactor/ssl/SSLIOSession.java
@@ -415,7 +415,15 @@ public class SSLIOSession implements IOSession, SessionBufferStatus, SocketAcces
             newMask = EventMask.READ;
             break;
         case NOT_HANDSHAKING:
-            newMask = this.appEventMask;
+            if (this.status >= CLOSING && isOutboundDone() && !isInboundDone()) {
+                // NEED_UNWRAP behavior
+                newMask = EventMask.READ;
+            } else if (this.status >= CLOSING && !isOutboundDone() && isInboundDone()) {
+                // NEED_WRAP behavior
+                newMask = EventMask.READ_WRITE;
+            } else {
+                newMask = this.appEventMask;
+            }
             break;
         case NEED_TASK:
             break;
