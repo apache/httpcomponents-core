bb8c36d61f3beaa912d51022e54d730146fb8f37
Johnny Lim <izeye@naver.com>
2024-11-01 23:07:54 +0900

    Add Javadoc since for NoConnectionReuseStrategy

--- httpcore5/src/main/java/org/apache/hc/core5/http/impl/NoConnectionReuseStrategy.java
+++ httpcore5/src/main/java/org/apache/hc/core5/http/impl/NoConnectionReuseStrategy.java
@@ -36,6 +36,7 @@ import org.apache.hc.core5.http.protocol.HttpContext;
 
 /**
  * A strategy that never reuses a connection.
+ * @since 5.4
  */
 @Contract(threading = ThreadingBehavior.IMMUTABLE)
 public class NoConnectionReuseStrategy implements ConnectionReuseStrategy {
