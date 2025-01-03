a83c22800271646a3756fca9718e2e8863dc3090
Oleg Kalnichevski <olegk@apache.org>
2025-01-03 09:58:35 +0100

    HTTPCORE-774: fixed a race condition caused by concurrent update of the connection input window to the max value.
    (cherry picked from commit d31d0000084d1ffdd37a9596b53e2aa7779451bc)

--- httpcore5-h2/src/main/java/org/apache/hc/core5/http2/impl/nio/AbstractH2StreamMultiplexer.java
+++ httpcore5-h2/src/main/java/org/apache/hc/core5/http2/impl/nio/AbstractH2StreamMultiplexer.java
@@ -1492,7 +1492,6 @@ abstract class AbstractH2StreamMultiplexer implements Identifiable, HttpConnecti
 
         @Override
         public void update(final int increment) throws IOException {
-            incrementInputCapacity(0, connInputWindow, increment);
             incrementInputCapacity(id, inputWindow, increment);
         }
 
