25861cbf68f502dca431b54b4a8df8f4f4f86aaf
Oleg Kalnichevski <olegk@apache.org>
2025-02-17 16:21:54 +0100

    Improved test case asserting the async client transport behavior when 100-continue confirmation message gets delayed

--- httpcore5-testing/src/test/java/org/apache/hc/core5/testing/nio/Http1IntegrationTest.java
+++ httpcore5-testing/src/test/java/org/apache/hc/core5/testing/nio/Http1IntegrationTest.java
@@ -135,6 +135,7 @@ import org.apache.hc.core5.http.support.BasicResponseBuilder;
 import org.apache.hc.core5.reactor.IOSession;
 import org.apache.hc.core5.reactor.ProtocolIOSession;
 import org.apache.hc.core5.testing.SSLTestContexts;
+import org.apache.hc.core5.testing.extension.ExecutorResource;
 import org.apache.hc.core5.testing.extension.nio.Http1TestResources;
 import org.apache.hc.core5.util.CharArrayBuffer;
 import org.apache.hc.core5.util.TextUtils;
@@ -150,13 +151,15 @@ abstract class Http1IntegrationTest {
     private static final Timeout LONG_TIMEOUT = Timeout.ofMinutes(2);
 
     private final URIScheme scheme;
-    private final ReentrantLock lock = new ReentrantLock();
     @RegisterExtension
     private final Http1TestResources resources;
+    @RegisterExtension
+    private final ExecutorResource executorResource;
 
     public Http1IntegrationTest(final URIScheme scheme) {
         this.scheme = scheme;
         this.resources = new Http1TestResources(scheme, TIMEOUT);
+        this.executorResource = new ExecutorResource(5);
     }
 
     private HttpHost target(final InetSocketAddress serverEndpoint) {
@@ -1096,15 +1099,19 @@ abstract class Http1IntegrationTest {
     }
 
     @Test
-    void testDelayedExpectationVerification() throws Exception {
+    void testDelayedExpectContinueAck() throws Exception {
         final Http1TestServer server = resources.server();
         final Http1TestClient client = resources.client();
+
+        // Disable 100-continue handshake on the server side
+        server.configure(handler -> handler);
 
         server.register("*", () -> new AsyncServerExchangeHandler() {
 
             private final Random random = new Random(System.currentTimeMillis());
             private final AsyncEntityProducer entityProducer = AsyncEntityProducers.create(
                     "All is well");
+            private final ReentrantLock lock = new ReentrantLock();
 
             @Override
             public void handleRequest(
@@ -1113,7 +1120,7 @@ abstract class Http1IntegrationTest {
                     final ResponseChannel responseChannel,
                     final HttpContext context) throws HttpException, IOException {
 
-                Executors.newSingleThreadExecutor().execute(() -> {
+                executorResource.getExecutorService().execute(() -> {
                     try {
                         if (entityDetails != null) {
                             final Header h = request.getFirstHeader(HttpHeaders.EXPECT);
@@ -1208,6 +1215,45 @@ abstract class Http1IntegrationTest {
             Assertions.assertNotNull(response);
             Assertions.assertEquals(200, response.getCode());
             Assertions.assertEquals("All is well", result.getBody());
+        }
+    }
+
+    @Test
+    void testMissingExpectContinueAckClientContinues() throws Exception {
+        final Http1TestServer server = resources.server();
+        final Http1TestClient client = resources.client();
+
+        // Disable 100-continue handshake on the server side
+        server.configure(handler -> handler);
+
+        server.register("/hello", () -> new SingleLineResponseHandler("Hi there back"));
+        final InetSocketAddress serverEndpoint = server.start();
+
+        final HttpHost target = target(serverEndpoint);
+
+        client.configure(Http1Config.custom()
+                .setWaitForContinueTimeout(Timeout.ofMilliseconds(100))
+                .build());
+        client.start();
+
+        final Future<ClientSessionEndpoint> connectFuture = client.connect(target, TIMEOUT);
+        final ClientSessionEndpoint streamEndpoint = connectFuture.get();
+
+        for (int i = 0; i < 5; i++) {
+            final BasicHttpRequest request = BasicRequestBuilder.post()
+                    .setHttpHost(target)
+                    .setPath("/hello")
+                    .build();
+            final Future<Message<HttpResponse, String>> future = streamEndpoint.execute(
+                    new BasicRequestProducer(request, AsyncEntityProducers.create("Hi there")),
+                    new BasicResponseConsumer<>(new StringAsyncEntityConsumer()), null);
+            final Message<HttpResponse, String> result = future.get(TIMEOUT.getDuration(), TIMEOUT.getTimeUnit());
+            Assertions.assertNotNull(result);
+            final HttpResponse response1 = result.getHead();
+            final String entity1 = result.getBody();
+            Assertions.assertNotNull(response1);
+            Assertions.assertEquals(200, response1.getCode());
+            Assertions.assertEquals("Hi there back", entity1);
         }
     }
 
