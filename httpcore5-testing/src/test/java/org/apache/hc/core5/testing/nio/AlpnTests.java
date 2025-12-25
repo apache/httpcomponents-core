527e3be45cb52ab2367e6f26c16584789cb69212
Ryan Schmitt <rschmitt@apache.org>
2025-12-25 12:22:59 -0500

    Disable Conscrypt tests on aarch64
    On OpenJDK, Conscrypt only supports Intel architectures. ARM builds are
    not available for Windows, Linux, or macOS.

--- httpcore5-testing/src/test/java/org/apache/hc/core5/testing/nio/AlpnTests.java
+++ httpcore5-testing/src/test/java/org/apache/hc/core5/testing/nio/AlpnTests.java
@@ -36,7 +36,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.condition.DisabledOnOs;
 import org.junit.jupiter.api.condition.EnabledForJreRange;
 import org.junit.jupiter.api.condition.JRE;
-import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.parallel.Isolated;
 
 class AlpnTests {
@@ -55,7 +54,7 @@ class AlpnTests {
     @Nested
     @Isolated
     @DisplayName("ALPN Conscrypt (Java 9 or newer)")
-    @DisabledOnOs(OS.MAC)
+    @DisabledOnOs(architectures = "aarch64")
     @EnabledForJreRange(min = JRE.JAVA_9)
     class ConscryptJSSEAlpnTest extends AlpnTest {
 
@@ -68,7 +67,7 @@ class AlpnTests {
     @Nested
     @Isolated
     @DisplayName("ALPN Conscrypt (Conscrypt specific TLS strategies)")
-    @DisabledOnOs(OS.MAC)
+    @DisabledOnOs(architectures = "aarch64")
     class ConscryptJSSEAndStrategiesAlpnTest extends AlpnTest {
 
         public ConscryptJSSEAndStrategiesAlpnTest() throws Exception {
@@ -76,5 +75,4 @@ class AlpnTests {
         }
 
     }
-
 }
