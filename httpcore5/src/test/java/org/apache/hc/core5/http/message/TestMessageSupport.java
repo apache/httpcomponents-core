a73b9a15d141a8d0f2ab7068bd6c1348f00fd201
Oleg Kalnichevski <olegk@apache.org>
2026-09-08 13:19:54 +0200

    Corrected the use of custom delimiters by #parseTokens in MessageSupport

--- httpcore5/src/test/java/org/apache/hc/core5/http/message/TestMessageSupport.java
+++ httpcore5/src/test/java/org/apache/hc/core5/http/message/TestMessageSupport.java
@@ -52,6 +52,7 @@ import org.apache.hc.core5.http.ProtocolException;
 import org.apache.hc.core5.http.io.entity.HttpEntities;
 import org.apache.hc.core5.http.support.BasicResponseBuilder;
 import org.apache.hc.core5.util.CharArrayBuffer;
+import org.apache.hc.core5.util.Tokenizer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -109,6 +110,15 @@ class TestMessageSupport {
         final List<String> tokens = new ArrayList<>();
         MessageSupport.parseTokens(s, cursor, tokens::add);
         Assertions.assertEquals(Arrays.asList("a", "b", "c", "c"), tokens);
+    }
+
+    @Test
+    void testParseTokensWithConsumerAndCustomDelimiter() {
+        final String s = "a, b ;  c, c";
+        final ParserCursor cursor = new ParserCursor(0, s.length());
+        final List<String> tokens = new ArrayList<>();
+        MessageSupport.parseTokens(s, cursor, Tokenizer.delimiters(';'), tokens::add);
+        Assertions.assertEquals(Arrays.asList("a, b", "c, c"), tokens);
     }
 
     @Test
